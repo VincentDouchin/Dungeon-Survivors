39980cc59911b1e6036c05cb4e14b078f293797a
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-24 09:39:29 +0100

    change main to start engine and initial game state

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,6 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import Engine from "./Globals/Engine"
+import Run from "./GameStates/Run"
+Engine.addState('run', Run)
+Engine.setState('run')
+Engine.start()
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
