1306b24e2eda467ca990d14ef43b8c7066c59ef0
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-24 16:59:41 +0100

    removed player creation

--- src/GameStates/Run.ts
+++ src/GameStates/Run.ts
@@ -1,27 +1,25 @@
-import { render } from "../Globals/Initialize"
+import { render, world } from "../Globals/Initialize"
 import PositionComponent from "../Components/PositionComponent"
 import { ECS, Entity } from "../Globals/ECS"
 import MeshComponent from "../Components/MeshComponent"
 import AssetManager from "../Globals/AssetManager"
 import RenderingSystem from "../Systems/RenderingSystem"
 import MovementSystem from "../Systems/MovementSystem"
-import PlayerControllerComponent from "../Components/PlayerControllerComponent"
+import PlayerEntity from "../Entities/PlayerEntity"
 const Run = new class implements GameState {
 	constructor() {
-		ECS.registerSystem(RenderingSystem)
-		ECS.registerSystem(MovementSystem)
-		const player = new Entity()
-		player.addComponent(new PositionComponent(0, 0))
-		player.addComponent(new PlayerControllerComponent())
-		const elf = AssetManager.tiles.get('elf_m_idle_anim')!
-		const knight = AssetManager.tiles.get('knight_f_idle_anim')!
-		player.addComponent(new MeshComponent(elf.buffer, elf.width, elf.height))
+		RenderingSystem.register()
+		MovementSystem.register()
+		PlayerEntity()
+		const knight = AssetManager.tiles.knight_f_idle_anim!
 		const npc = new Entity()
 		npc.addComponent(new PositionComponent(10, 10))
 		npc.addComponent(new MeshComponent(knight.buffer))
 	}
 	update() {
+		world.step()
 		ECS.updateSystems()
+
 	}
 	render() {
 		render()
