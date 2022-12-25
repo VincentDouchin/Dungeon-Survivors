b880e4787d26c28f8723d36f4495b28746a05679
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-25 10:45:10 +0100

    added animation component

--- src/Entities/PlayerEntity.ts
+++ src/Entities/PlayerEntity.ts
@@ -1,3 +1,4 @@
+import AnimationComponent from "../Components/AnimationComponent"
 import BodyComponent from "../Components/BodyComponent"
 import MeshComponent from "../Components/MeshComponent"
 import PlayerControllerComponent from "../Components/PlayerControllerComponent"
@@ -9,6 +10,12 @@ const PlayerEntity = () => {
 	const player = new Entity()
 	const elf = AssetManager.tiles.elf_f_idle_anim
 	player.addComponent(new MeshComponent(elf.buffer, elf.width, elf.height))
+
+	player.addComponent(new AnimationComponent({
+		idle: AssetManager.tiles.elf_f_idle_anim,
+		hit: AssetManager.tiles.elf_f_hit_anim,
+		run: AssetManager.tiles.elf_f_run_anim,
+	}))
 	player.addComponent(new BodyComponent({ moveForce: 100 }))
 	player.addComponent(new PositionComponent(0, 0))
 	player.addComponent(new PlayerControllerComponent())
