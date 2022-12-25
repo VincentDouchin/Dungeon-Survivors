7e695fb35846a9ac1fc9ed32ae643c29627e7301
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-25 17:18:40 +0100

    flip mesh if going left

--- src/Systems/AnimationSystem.ts
+++ src/Systems/AnimationSystem.ts
@@ -1,4 +1,5 @@
 import AnimationComponent from "../Components/AnimationComponent";
+
 import MeshComponent from "../Components/MeshComponent";
 import { Entity, System } from "../Globals/ECS";
 
@@ -10,15 +11,15 @@ class AnimationSystem extends System {
 		entities.forEach(entity => {
 			const animation = entity.getComponent(AnimationComponent)
 			const mesh = entity.getComponent(MeshComponent)
-
 			animation.frameCounter++
 			if (animation.frameCounter > animation.frameRate) {
 				animation.frameCounter = 0
 				animation.selectedFrame = (animation.selectedFrame + 1) % animation.frames
 			}
 
-			mesh.texture.repeat.set(1 / animation.maxFrames, 1)
-			mesh.texture.offset.set(animation.selectedFrame / animation.frames, 0)
+			// mesh.texture.repeat.set(1 / animation.maxFrames, 1)
+			mesh.texture.repeat.set((animation.flipped ? -1 : 1) / animation.maxFrames, 1)
+			mesh.texture.offset.set(((animation.flipped ? 1 : 0) + animation.selectedFrame) / animation.frames, 0)
 			if (animation.currentState != animation.lastState) {
 				mesh.texture.image = animation.tile.buffer.canvas
 				mesh.texture.needsUpdate = true
