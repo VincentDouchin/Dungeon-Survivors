f7d5cab3b531e1d05bd9a4b9055ca56619fd9765
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-25 17:19:11 +0100

    start of health system

--- src/GameStates/Run.ts
+++ src/GameStates/Run.ts
@@ -1,20 +1,21 @@
 import { render, world } from "../Globals/Initialize"
-import PositionComponent from "../Components/PositionComponent"
-import { ECS, Entity } from "../Globals/ECS"
-import MeshComponent from "../Components/MeshComponent"
-import AssetManager from "../Globals/AssetManager"
+import { ECS } from "../Globals/ECS"
 import RenderingSystem from "../Systems/RenderingSystem"
 import MovementSystem from "../Systems/MovementSystem"
 import PlayerEntity from "../Entities/PlayerEntity"
+import AnimationSystem from "../Systems/AnimationSystem"
+import EnemyEntity from "../Entities/EnemyEntity"
+import AIControllerComponent from "../Components/AIControllerComponent"
+import HealthSystem from "../Systems/HealthSystem"
 const Run = new class implements GameState {
 	constructor() {
 		RenderingSystem.register()
 		MovementSystem.register()
-		PlayerEntity()
-		const knight = AssetManager.tiles.knight_f_idle_anim!
-		const npc = new Entity()
-		npc.addComponent(new PositionComponent(10, 10))
-		npc.addComponent(new MeshComponent(knight.buffer))
+		AnimationSystem.register()
+		HealthSystem.register()
+		const player = PlayerEntity()
+		const enemy = EnemyEntity()
+		enemy.getComponent(AIControllerComponent).target = player
 	}
 	update() {
 		world.step()
