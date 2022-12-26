2b4e8bd96d65a80fbf452ae495e18e15e05e8402
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-26 07:50:10 +0100

    added config for collider and enabled sensor

--- src/Components/BodyComponent.ts
+++ src/Components/BodyComponent.ts
@@ -6,13 +6,20 @@ interface bodyOptions {
 	type?: 'dynamic' | 'fixed'
 	moveForce?: number
 }
-
+interface colliderOptions {
+	contact: boolean
+	width: number
+	height: number
+	sensor?: boolean
+}
 class BodyComponent extends Component {
 	body: RigidBody
 	collider: Collider
 	moveForce: number
-	constructor(bodyOptions: bodyOptions, contact: boolean) {
+	sensor: boolean
+	constructor(bodyOptions: bodyOptions, colliderOptions: colliderOptions) {
 		super()
+		this.sensor = colliderOptions.sensor ?? false
 		this.moveForce = bodyOptions.moveForce ?? 10
 		//!Body
 		const bodyDescription = RigidBodyDesc[bodyOptions.type ?? 'dynamic']()
@@ -24,20 +31,23 @@ class BodyComponent extends Component {
 		//!Collider
 		const colliderDescription =
 			ColliderDesc
-				.cuboid(8, 8)
-				.setDensity(1)
-		if (contact) {
+				.cuboid(colliderOptions.width / 2, colliderOptions.height / 2)
+				.setDensity(1000)
+				.setSensor(this.sensor)
+		if (colliderOptions.contact) {
 			colliderDescription.setActiveEvents(ActiveEvents.COLLISION_EVENTS)
 		}
 		// .setActiveCollisionTypes(ActiveCollisionTypes.DEFAULT | ActiveCollisionTypes.KINEMATIC_FIXED)
 		this.collider = world.createCollider(colliderDescription, this.body)
 	}
 	contacts(fn: Function) {
-		world.contactsWith(this.collider, (otherCollider) => {
+		const touchCollider = (otherCollider: Collider) => {
 			const entityId = otherCollider?.parent()?.userData as string
 			const entity = ECS.getEntityById(entityId)
 			fn(entity)
-		})
+		}
+		world.contactsWith(this.collider, touchCollider)
+		world.intersectionsWith(this.collider, touchCollider)
 	}
 	bind(id: string) {
 		this.body.userData = id
