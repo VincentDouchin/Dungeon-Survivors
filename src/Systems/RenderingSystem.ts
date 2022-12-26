5bf0b03f1111505c0887efd9f0607ea486a1fa40
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-26 09:54:22 +0100

    rotate mesh according to body

--- src/Systems/RenderingSystem.ts
+++ src/Systems/RenderingSystem.ts
@@ -1,5 +1,7 @@
+import BodyComponent from "../Components/BodyComponent";
 import MeshComponent from "../Components/MeshComponent";
 import PositionComponent from "../Components/PositionComponent";
+import WeaponControllerComponent from "../Components/WeaponControllerComponent";
 import { Entity, System } from "../Globals/ECS";
 import { scene } from "../Globals/Initialize";
 
@@ -11,12 +13,18 @@ class RenderingSystem extends System {
 		entities.forEach(entity => {
 			const mesh = entity.getComponent(MeshComponent)
 			const position = entity.getComponent(PositionComponent)
+			const body = entity.getComponent(BodyComponent)
+			const weaponController = entity.getComponent(WeaponControllerComponent)
 			if (mesh && position) {
 
 				if (!mesh.mesh.parent) {
 					scene.add(mesh.mesh)
 				}
 				mesh.mesh.position.set(position.x, position.y, 0)
+			}
+			if (weaponController?.owner && body.body) {
+				mesh.mesh.rotation.z = body.body.rotation() + Math.PI / 2
+
 			}
 		})
 	}
