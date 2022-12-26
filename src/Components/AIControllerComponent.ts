1e794f59886ccf8b9ade173f37cb54c3113e0a81
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-26 10:55:01 +0100

    reset target if destroyed

--- src/Components/AIControllerComponent.ts
+++ src/Components/AIControllerComponent.ts
@@ -1,10 +1,18 @@
-import { Component, Entity } from "../Globals/ECS";
+import ECSEVENTS from "../Constants/ECSEvents";
+import { Component, ECS, Entity } from "../Globals/ECS";
 
 class AIControllerComponent extends Component {
 	enabled = true
 	target: Entity | null = null
-	constructor() {
+	constructor(target: Entity) {
 		super()
+		this.target = target
+		ECS.eventBus.subscribe(ECSEVENTS.DELETEENTITY, (entityId: string) => {
+
+			if (entityId == this.target?.id) {
+				this.target = null
+			}
+		})
 	}
 }
 AIControllerComponent.register()
