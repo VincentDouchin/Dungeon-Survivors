131719194975f381e9c40c701c445c0129b7bffe
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-27 06:54:52 +0100

    change color on damage taken

--- src/Systems/HealthSystem.ts
+++ src/Systems/HealthSystem.ts
@@ -2,7 +2,9 @@ import BodyComponent from "../Components/BodyComponent";
 import DamageComponent from "../Components/DamageComponent";
 import HealthComponent from "../Components/HealthComponent";
 import MeshComponent from "../Components/MeshComponent";
+import Coroutines from "../Globals/Coroutines";
 import { Entity, System } from "../Globals/ECS";
+import waitFor from "../Utils/WaitFor";
 
 class HealthSystem extends System {
 	constructor() {
@@ -11,18 +13,27 @@ class HealthSystem extends System {
 	update(entities: Entity[]) {
 		entities.forEach(entity => {
 			const health = entity.getComponent(HealthComponent)
-			const sprite = entity.getComponent(MeshComponent)
+			const mesh = entity.getComponent(MeshComponent)
 			const body = entity.getComponent(BodyComponent)
-			if (!health.mesh.parent && sprite) {
-				health.mesh.scale.x = sprite.width / health.width
-				sprite.mesh.add(health.mesh)
-				health.mesh.position.set(0, sprite.width / 2, 0)
+			if (!health.mesh.parent && mesh) {
+				health.mesh.scale.x = mesh.width / health.width
+				mesh.mesh.add(health.mesh)
+				health.mesh.position.set(0, mesh.width / 2, 0)
 			}
 			if (body) {
 				body.contacts((otherEntity: Entity) => {
 					const damage = otherEntity.getComponent(DamageComponent)
 					if (damage.target != health.type) {
 						health.updateHealth(-damage.amount)
+						if (mesh) {
+							mesh.uniforms.uColor.value.x = 0.7
+							Coroutines.add(function* () {
+								yield* waitFor(10)
+								mesh.uniforms.uColor.value.x = 0
+
+							})
+						}
+
 					}
 				})
 			}
