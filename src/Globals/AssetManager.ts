80899a1b04a8768ce175136a738ec39ff3e4144d
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-27 06:54:22 +0100

    changed asset loading to unique tile class

--- src/Globals/AssetManager.ts
+++ src/Globals/AssetManager.ts
@@ -1,6 +1,8 @@
 import getBuffer from '../Utils/Buffer'
 import imageSource from './../../assets/0x72_DungeonTilesetII_v1.4.png'
+import normalsSource from './../../assets/0x72_DungeonTilesetII_v1.4-normals.png'
 import tilesList from './../../assets/tiles_list_v1.4.txt?raw'
+import Tile from '../Utils/Tile'
 
 const loadImage = async (source: string) => {
 	const img = new Image()
@@ -12,14 +14,17 @@ const loadImage = async (source: string) => {
 
 }
 const img = await loadImage(imageSource)
+const normals = await loadImage(normalsSource)
 const UISources = import.meta.glob('./../../assets/UI/*.png', { eager: true })
-const UIImages: Record<string, CanvasRenderingContext2D> = {}
+const UIImages: Record<string, Tile> = {}
 for (let path of Object.values(UISources).map((module: any) => module.default)) {
 	const fileName = path.split('/').at(-1).split('.').at(-2)
 	const image = await loadImage(path)
 	const buffer = getBuffer(image.width, image.height)
 	buffer.drawImage(image, 0, 0)
-	UIImages[fileName] = buffer
+	UIImages[fileName] = new Tile({
+		buffer: buffer
+	})
 }
 
 const tiles = tilesList
@@ -29,15 +34,17 @@ const tiles = tilesList
 	.reduce((acc, [tileName, top, left, width, height, frames = '1']) => {
 		const totalWidth = parseInt(width) * parseInt(frames)
 		const buffer = getBuffer(totalWidth, parseInt(height))
+		const normalMap = getBuffer(totalWidth, parseInt(height))
 		buffer.drawImage(img, parseInt(top), parseInt(left), totalWidth, parseInt(height), 0, 0, totalWidth, parseInt(height))
-		const tile: Tile = {
+		normalMap.drawImage(normals, parseInt(top), parseInt(left), totalWidth, parseInt(height), 0, 0, totalWidth, parseInt(height))
+
+		const tile: Tile = new Tile({
 			buffer,
-			x: Number(top),
-			y: Number(left),
+			normalMap,
 			width: Number(width),
 			height: Number(height),
 			frames: Number(frames)
-		}
+		})
 		return { ...acc, [tileName]: tile }
 
 	}, {})
@@ -45,6 +52,6 @@ const tiles = tilesList
 const AssetManager = new class {
 	image = img
 	tiles = tiles as Record<tileName, Tile>
-	UI = UIImages
+	UI = UIImages as Record<string, Tile>
 }
 export default AssetManager
