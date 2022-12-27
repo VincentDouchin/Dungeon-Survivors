402178a30e79bee39565912734c0e2064588287d
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-27 06:54:07 +0100

    xp en brazier

--- src/GameStates/Run.ts
+++ src/GameStates/Run.ts
@@ -1,18 +1,18 @@
-import { camera, render, world } from "../Globals/Initialize"
+import { render, world } from "../Globals/Initialize"
 import { ECS } from "../Globals/ECS"
 import RenderingSystem from "../Systems/RenderingSystem"
 import MovementSystem from "../Systems/MovementSystem"
-import PlayerEntity from "../Entities/PlayerEntity"
 import AnimationSystem from "../Systems/AnimationSystem"
-
 import HealthSystem from "../Systems/HealthSystem"
 import BodyCreationSystem from "../Systems/BodyCreationSystem"
-import PositionComponent from "../Components/PositionComponent"
+
+import PlayerEntity from "../Entities/PlayerEntity"
 import BackgroundEntity from "../Entities/BackgroundEntity"
-import { Vector3 } from "three"
+import BrasierEntity from "../Entities/BrasierEntity"
 import startWave from "../Game/Wave"
 import Enemies from "../Constants/Enemies"
-import BrasierEntity from "../Entities/BrasierEntity"
+import XPEntity from "../Entities/XPEntity"
+import PositionComponent from "../Components/PositionComponent"
 const Run = (): GameState => {
 
 	RenderingSystem.register()
@@ -25,6 +25,9 @@ const Run = (): GameState => {
 	BackgroundEntity()
 	startWave(player)([Enemies.orc, 10, 20], [Enemies.orcShaman, 10, 10])
 	BrasierEntity({ x: 0, y: 0 })
+	BrasierEntity({ x: 100, y: 0 })
+	const xp = XPEntity()
+	xp.addComponent(new PositionComponent(5, 5))
 	return {
 		update() {
 			world.step()
