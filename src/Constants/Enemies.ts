d6e2805c6decf00f544b5d9bd4d052a6fc99cb41
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-27 18:01:19 +0100

    added goblins

--- src/Constants/Enemies.ts
+++ src/Constants/Enemies.ts
@@ -5,28 +5,35 @@ const Enemies: Record<string, EnemyType> = {
 			idle: AssetManager.tiles.orc_warrior_idle_anim,
 			run: AssetManager.tiles.orc_warrior_run_anim
 		},
-		health: 10,
+		health: 30,
 	},
 	orcShaman: {
 		tiles: {
 			idle: AssetManager.tiles.orc_shaman_idle_anim,
 			run: AssetManager.tiles.orc_shaman_run_anim
 		},
-		health: 20,
+		health: 50,
 	},
 	orcMasked: {
 		tiles: {
 			idle: AssetManager.tiles.masked_orc_idle_anim,
 			run: AssetManager.tiles.masked_orc_run_anim,
 		},
-		health: 30
+		health: 80
+	},
+	goblin: {
+		tiles: {
+			idle: AssetManager.tiles.goblin_idle_anim,
+			run: AssetManager.tiles.goblin_run_anim
+		},
+		health: 10
 	},
 	zombieBig: {
 		tiles: {
 			idle: AssetManager.tiles.big_zombie_idle_anim,
 			run: AssetManager.tiles.big_zombie_run_anim,
 		},
-		health: 100
+		health: 300
 	}
 
 }
