fd72046fe2bd1ddb531b799d13c9ed0a8d062fd3
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-27 15:23:56 +0100

    generic function to update bar mesh

---
+++ src/UIEntities.ts/UpdateBar.ts
@@ -0,0 +1,11 @@
+import MeshComponent from '../Components/MeshComponent'
+import Tile from '../Utils/Tile'
+const updateBar = (mesh: MeshComponent, from: Tile, to: Tile, percent: number) => {
+	const fullWidth = Math.ceil(percent * mesh.width)
+	const image: HTMLCanvasElement = mesh.texture.image
+	const ctx = image.getContext('2d')
+	ctx!.drawImage(from.buffer.canvas, 0, 0)
+	ctx!.drawImage(to.buffer.canvas, 0, 0, fullWidth, mesh.height, 0, 0, fullWidth, mesh.height)
+	mesh.uniforms.uTexture.value.needsUpdate = true
+}
+export default updateBar
