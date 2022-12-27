e6f58d613088bf309f69072bef604072e7cabebe
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-27 10:11:47 +0100

    changed UI images to aseprite data

--- src/Components/HealthComponent.ts
+++ src/Components/HealthComponent.ts
@@ -1,31 +1,29 @@
 import AssetManager from "../Globals/AssetManager";
-
 import MeshComponent from "./MeshComponent";
-const full = AssetManager.UI['Health-full']
-const empty = AssetManager.UI['Health-empty']
+const full = AssetManager.UI['healthFull']
+const empty = AssetManager.UI['healthBar']
 class HealthComponent extends MeshComponent {
 	health: number
 	maxHealth: number
-	type: DamageType
-	constructor(health: number, type: DamageType) {
-
-		super(full.clone(), { renderOrder: 11 })
-		this.updateHealth(0)
+	type: number
+	constructor(health: number, type: number) {
+		const tile = empty.clone()
+		super(tile, { renderOrder: 11 })
 		this.health = health
 		this.maxHealth = health
+		this.updateHealth(0)
 		this.type = type
 	}
 	updateHealth(amount: number) {
 		this.health = Math.max(0, Math.min(this.health + amount, this.maxHealth))
 		const healthPercent = this.health / this.maxHealth
 		const fullWidth = Math.ceil(healthPercent * this.width)
-		const emptyWidth = Math.floor(this.width - fullWidth)
 		const image: HTMLCanvasElement = this.texture.image
 		const ctx = image.getContext('2d')
-		ctx?.drawImage(empty.buffer.canvas, fullWidth, 0, emptyWidth, this.height, fullWidth, 0, emptyWidth, this.height)
+		// ctx?.clearRect()
+		ctx?.drawImage(empty.buffer.canvas, 0, 0)
 		ctx?.drawImage(full.buffer.canvas, 0, 0, fullWidth, this.height, 0, 0, fullWidth, this.height)
-		this.texture.needsUpdate = true
-
+		this.uniforms.uTexture.value.needsUpdate = true
 	}
 }
 HealthComponent.register()
--- src/Globals/AssetManager.ts
+++ src/Globals/AssetManager.ts
@@ -3,6 +3,8 @@ import imageSource from './../../assets/0x72_DungeonTilesetII_v1.4.png'
 import normalsSource from './../../assets/0x72_DungeonTilesetII_v1.4-normals.png'
 import tilesList from './../../assets/tiles_list_v1.4.txt?raw'
 import Tile from '../Utils/Tile'
+import GUISource from './../../assets/GUI.png'
+import GUIData from './../../assets/GUI.json'
 
 const loadImage = async (source: string) => {
 	const img = new Image()
@@ -15,17 +17,14 @@ const loadImage = async (source: string) => {
 }
 const img = await loadImage(imageSource)
 const normals = await loadImage(normalsSource)
-const UISources = import.meta.glob('./../../assets/UI/*.png', { eager: true })
-const UIImages: Record<string, Tile> = {}
-for (let path of Object.values(UISources).map((module: any) => module.default)) {
-	const fileName = path.split('/').at(-1).split('.').at(-2)
-	const image = await loadImage(path)
-	const buffer = getBuffer(image.width, image.height)
-	buffer.drawImage(image, 0, 0)
-	UIImages[fileName] = new Tile({
-		buffer: buffer
-	})
-}
+const UI = await loadImage(GUISource)
+const UIImages = GUIData.meta.slices.reduce((acc, slice) => {
+
+	const { w, h, x, y }: Record<string, number> = slice.keys[0].bounds
+	const buffer = getBuffer(w, h)
+	buffer.drawImage(UI, x, y, w, h, 0, 0, w, h)
+	return { ...acc, [slice.name]: new Tile({ buffer }) }
+}, {})
 
 const tiles = tilesList
 	.split('\n')
