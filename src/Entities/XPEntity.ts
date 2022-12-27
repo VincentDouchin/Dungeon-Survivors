1209e0b759f8ac10a63398f7b7cab32465b10451
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-27 06:51:37 +0100

    added xp and normals

---
+++ src/Entities/XPEntity.ts
@@ -0,0 +1,11 @@
+import MeshComponent from "../Components/MeshComponent"
+import AssetManager from "../Globals/AssetManager"
+import { Entity } from "../Globals/ECS"
+
+const XPEntity = () => {
+	const xp = new Entity()
+	xp.addComponent(new MeshComponent(AssetManager.tiles.xp, { renderOrder: 1 }))
+
+	return xp
+}
+export default XPEntity
