26cb5abb57363a5cb3ae94dc36ff9299ea9af857
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-27 10:14:59 +0100

    added distance from parent

--- src/Components/WeaponControllerComponent.ts
+++ src/Components/WeaponControllerComponent.ts
@@ -4,9 +4,11 @@ import { Component, ECS, Entity } from "../Globals/ECS";
 class WeaponControllerComponent extends Component {
 	ownerId: null | string = null
 	joint: ImpulseJoint | null = null
-	constructor(owner: Entity) {
+	distance: number
+	constructor(owner: Entity, distance: number = 30) {
 		super()
 		this.ownerId = owner.id
+		this.distance = distance
 	}
 	get owner() {
 		return this.ownerId ? ECS.getEntityById(this.ownerId) : undefined
