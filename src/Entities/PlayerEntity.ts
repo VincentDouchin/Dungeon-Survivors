4aa3ec00f906780b659f38d0f7d65bf86efe5ca0
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-28 09:15:21 +0100

    added potion

--- src/Entities/PlayerEntity.ts
+++ src/Entities/PlayerEntity.ts
@@ -1,6 +1,7 @@
 import AnimationComponent from "../Components/AnimationComponent"
 import BodyComponent from "../Components/BodyComponent"
 import CameraTargetComponent from "../Components/CameraTargetComponent"
+import DamageComponent from "../Components/DamageComponent"
 import HealthComponent from "../Components/HealthComponent"
 import LightComponent from "../Components/LightComponent"
 import MeshComponent from "../Components/MeshComponent"
@@ -26,11 +27,12 @@ const PlayerEntity = () => {
 	player.addComponent(new BodyComponent(
 		{ moveForce: 100 },
 		[
-			{ width: elf.width, height: elf.height, contact: true, group: COLLISIONGROUPS.PLAYER, canCollideWith: [COLLISIONGROUPS.ENEMY, COLLISIONGROUPS.TRAP] },
+			{ width: elf.width, height: elf.height, contact: true, group: COLLISIONGROUPS.PLAYER, canCollideWith: [COLLISIONGROUPS.ENEMY, COLLISIONGROUPS.TRAP, COLLISIONGROUPS.POTION] },
 			{ width: 100, height: 100, contact: true, sensor: true, group: COLLISIONGROUPS.SENSOR, canCollideWith: [COLLISIONGROUPS.XP] }
 		]
 
 	))
+	player.addComponent(new DamageComponent(1, COLLISIONGROUPS.PLAYER))
 	player.addComponent(new PositionComponent(0, 0))
 	player.addComponent(new PlayerControllerComponent())
 	player.addComponent(new CameraTargetComponent())
