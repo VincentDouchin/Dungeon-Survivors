c2bf9507d35b325a572244dbe9dbadf57d504870
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-28 14:11:53 +0100

    add combine shader

--- src/Shaders/LigthShader.ts
+++ src/Shaders/LigthShader.ts
@@ -3,13 +3,17 @@ import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer
 import lightFrag from './frag/lights.frag?raw'
 
 import mainVert from './vert/main.vert?raw'
-const LightShader = (mainComposer: EffectComposer, lightComposer: EffectComposer) => new ShaderMaterial({
-	uniforms: {
-		main_texture: new Uniform(mainComposer.renderTarget2.texture),
-		light_texture: new Uniform(lightComposer.renderTarget2.texture)
-	},
-	vertexShader: mainVert,
-	fragmentShader: lightFrag,
+const LightShader = (mainComposer: EffectComposer, lightComposer: EffectComposer) => {
+	const material = new ShaderMaterial({
+		uniforms: {
+			main_texture: new Uniform(mainComposer.renderTarget2.texture),
+			light_texture: new Uniform(lightComposer.renderTarget2.texture)
+		},
+		vertexShader: mainVert,
+		fragmentShader: lightFrag,
 
-})
+	})
+	material.transparent = true
+	return material
+}
 export default LightShader
