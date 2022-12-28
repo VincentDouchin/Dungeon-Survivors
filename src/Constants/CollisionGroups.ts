37299c40286c764a119a31784ebb8689f3e5b2e8
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-28 09:15:03 +0100

    added potion

--- src/Constants/CollisionGroups.ts
+++ src/Constants/CollisionGroups.ts
@@ -1,9 +1,10 @@
 const COLLISIONGROUPS = {
-	PLAYER: 64,
 	ENEMY: 2,
 	XP: 4,
 	WEAPON: 8,
 	SENSOR: 16,
-	TRAP: 32
+	TRAP: 32,
+	PLAYER: 64,
+	POTION: 128
 }
 export default COLLISIONGROUPS
---
+++ src/Entities/PotionEntity.ts
@@ -0,0 +1,22 @@
+import BodyComponent from "../Components/BodyComponent"
+import DamageComponent from "../Components/DamageComponent"
+import HealthComponent from "../Components/HealthComponent"
+import MeshComponent from "../Components/MeshComponent"
+import PositionComponent from "../Components/PositionComponent"
+import COLLISIONGROUPS from "../Constants/CollisionGroups"
+import AssetManager from "../Globals/AssetManager"
+import { Entity } from "../Globals/ECS"
+
+const PotionEntity = ({ x, y }: { x: number, y: number }) => {
+	const potion = new Entity()
+	const tile = AssetManager.tiles.flask_big_green
+	potion.addComponent(new MeshComponent(tile))
+	potion.addComponent(new BodyComponent({ type: 'fixed' }, [
+		{ width: tile.width, height: tile.height, sensor: false, contact: true, group: COLLISIONGROUPS.POTION, canCollideWith: [COLLISIONGROUPS.PLAYER] }
+	]))
+	potion.addComponent(new DamageComponent(-50, COLLISIONGROUPS.POTION))
+	potion.addComponent(new PositionComponent(x, y))
+	potion.addComponent(new HealthComponent(1, COLLISIONGROUPS.POTION))
+	return potion
+}
+export default PotionEntity
