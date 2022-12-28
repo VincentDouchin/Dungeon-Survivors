acee1399ae3be3c4a89df9b80e58689a2be84639
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-28 11:50:34 +0100

    changed weapon creation

--- src/Entities/WeaponEntity.ts
+++ src/Entities/WeaponEntity.ts
@@ -3,18 +3,17 @@ import DamageComponent from "../Components/DamageComponent"
 import MeshComponent from "../Components/MeshComponent"
 import WeaponControllerComponent from "../Components/WeaponControllerComponent"
 import COLLISIONGROUPS from "../Constants/CollisionGroups"
-import AssetManager from "../Globals/AssetManager"
 import { Entity } from "../Globals/ECS"
 
-const WeaponEntity = (owner: Entity) => {
+const WeaponEntity = (owner: Entity) => (weaponDefinition: WeaponDefinition) => {
 	const weapon = new Entity()
-	const sword = AssetManager.tiles.weapon_knight_sword
+	const tile = weaponDefinition.tile
 	weapon.addComponent(new BodyComponent(
 		{ moveForce: 10 },
-		[{ width: sword.width, height: sword.height, contact: true, sensor: true, mass: 1, group: COLLISIONGROUPS.WEAPON, canCollideWith: [COLLISIONGROUPS.ENEMY] }]
+		[{ width: tile.width, height: tile.height, contact: true, sensor: true, mass: 1, group: COLLISIONGROUPS.WEAPON, canCollideWith: [COLLISIONGROUPS.ENEMY] }]
 	))
-	weapon.addComponent(new MeshComponent(sword))
-	weapon.addComponent(new DamageComponent(5, COLLISIONGROUPS.ENEMY))
+	weapon.addComponent(new MeshComponent(tile))
+	weapon.addComponent(new DamageComponent(weaponDefinition.damage, [COLLISIONGROUPS.ENEMY]))
 	weapon.addComponent(new WeaponControllerComponent(owner))
 	return weapon
 }
