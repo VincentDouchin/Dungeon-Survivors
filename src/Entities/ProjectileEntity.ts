6aac91359448a66d0ada2983788ad32e6322551f
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-29 10:34:34 +0100

    added more options for range weapons

--- src/Entities/ProjectileEntity.ts
+++ src/Entities/ProjectileEntity.ts
@@ -1,14 +1,20 @@
+import AnimationComponent from "../Components/AnimationComponent"
 import BodyComponent from "../Components/BodyComponent"
 import DamageComponent from "../Components/DamageComponent"
 import MeshComponent from "../Components/MeshComponent"
 import PositionComponent from "../Components/PositionComponent"
 import RotationComponent from "../Components/RotationComponent"
 import COLLISIONGROUPS from "../Constants/CollisionGroups"
+import Coroutines from "../Globals/Coroutines"
 import { Entity } from "../Globals/ECS"
+import waitFor from "../Utils/WaitFor"
 
-const ProjectileEntity = (projectileDefinition: ProjectileDefinition, position: { x: number, y: number }, rotation: number) => {
+const ProjectileEntity = (projectileDefinition: ProjectileDefinition, position: { x: number, y: number }, rotation: number, range: number) => {
 	const projectile = new Entity()
 	projectile.addComponent(new MeshComponent(projectileDefinition.tile))
+	if (projectileDefinition.tile.frames > 1) {
+		projectile.addComponent(new AnimationComponent({ idle: projectileDefinition.tile }))
+	}
 	projectile.addComponent(new BodyComponent(
 		{ moveForce: projectileDefinition.speed },
 		[
@@ -18,6 +24,10 @@ const ProjectileEntity = (projectileDefinition: ProjectileDefinition, position: 
 	projectile.addComponent(new DamageComponent(projectileDefinition.damage, [COLLISIONGROUPS.ENEMY], 1))
 	projectile.addComponent(new PositionComponent(position.x, position.y))
 	projectile.addComponent(new RotationComponent(rotation, 0))
+	Coroutines.add(function* () {
+		yield* waitFor(range)
+		projectile.destroy()
+	})
 	return projectile
 }
 export default ProjectileEntity
