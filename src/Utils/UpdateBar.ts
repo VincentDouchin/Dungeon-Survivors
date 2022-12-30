c61851cd1f4a3cd5ececf83756a8845435042b79
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-30 13:34:55 +0100

    fix for transparent textures

--- src/Utils/UpdateBar.ts
+++ src/Utils/UpdateBar.ts
@@ -1,11 +1,13 @@
 import MeshComponent from '../Components/MeshComponent'
+import getBuffer from './Buffer'
 import Tile from './Tile'
 const updateBar = (mesh: MeshComponent, from: Tile, to: Tile, percent: number) => {
 	const image: HTMLCanvasElement = mesh.texture.image
-	const fullWidth = Math.ceil(percent * mesh.texture.image.width)
-	const ctx = image.getContext('2d')
-	ctx!.drawImage(from.buffer.canvas, 0, 0,)
-	ctx!.drawImage(to.buffer.canvas, 0, 0, fullWidth, mesh.height, 0, 0, fullWidth, mesh.height)
+	const fullWidth = percent * mesh.texture.image.width
+	const ctx = image.getContext('2d')!
+	ctx.clearRect(0, 0, mesh.width, mesh.height)
+	ctx.drawImage(from.buffer.canvas, 0, 0,)
+	ctx.drawImage(to.buffer.canvas, 0, 0, fullWidth, mesh.height, 0, 0, fullWidth, mesh.height)
 	mesh.texture.needsUpdate = true
 }
 export default updateBar
