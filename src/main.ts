c70d1fbc7f6f968cebe7b0cc51e7c81f30183272
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-30 09:26:21 +0100

    added levelup state

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import Engine from "./Globals/Engine"
-import Run from "./GameStates/Run"
-Engine.addState('run', Run())
+import Run from "./GameStates/RunState"
+import LevelUp from "./GameStates/LevelUpState"
+//@ts-ignore
+import { preloadFont } from 'troika-three-text'
+
+await new Promise<void>((resolve) => preloadFont(
+	{ font: '../assets/fonts/m5x7.ttf', }, () => resolve()
+))
+Engine.addState('run', new Run())
+Engine.addState('levelUp', new LevelUp())
 Engine.setState('run')
 Engine.start()
 
