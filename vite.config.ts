d48c5fd51b4bbab2d3534fe5d612b5eaa4660591
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-30 11:55:03 +0100

    added vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
-
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
 	plugins: [
+		VitePWA({ registerType: 'autoUpdate' })
 	],
 	base: "/Dungeon-Survivor/",
 	build: {
 		target: 'esnext'
 	},
-
+	server: {
+		host: true
+	}
 })
