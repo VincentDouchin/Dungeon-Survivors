11523fecf50ac17748436f4abdba69c41fc018bd
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-30 09:25:12 +0100

    removed damage component

--- src/Entities/PlayerEntity.ts
+++ src/Entities/PlayerEntity.ts
@@ -2,7 +2,6 @@ import { Color } from "three"
 import AnimationComponent from "../Components/AnimationComponent"
 import BodyComponent from "../Components/BodyComponent"
 import CameraTargetComponent from "../Components/CameraTargetComponent"
-import DamageComponent from "../Components/DamageComponent"
 import HealthComponent from "../Components/HealthComponent"
 import LightComponent from "../Components/LightComponent"
 import MeshComponent from "../Components/MeshComponent"
@@ -19,10 +18,11 @@ const PlayerEntity = (hero: HeroDefinition, main: Entity | false, weapon: Weapon
 
 	player.addComponent(new MeshComponent(hero.tiles.idle))
 	player.addComponent(new LightComponent(new Color('hsl(0,0%,5%)'), 1000))
-	player.addComponent(new AnimationComponent(hero.tiles))
 	player.addComponent(new HealthComponent(200, COLLISIONGROUPS.PLAYER))
+	player.addComponent(new AnimationComponent(hero.tiles))
 	if (main) {
 		player.addComponent(new TargeterComponent(main.id, 50))
+
 	}
 	player.addComponent(new BodyComponent(
 		{ moveForce: 100 },
@@ -32,7 +32,6 @@ const PlayerEntity = (hero: HeroDefinition, main: Entity | false, weapon: Weapon
 		]
 
 	))
-	player.addComponent(new DamageComponent(1, [COLLISIONGROUPS.POTION]))
 	player.addComponent(new PositionComponent(0, 0))
 
 	if (!main) {
