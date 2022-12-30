92094e81375ef8f8e6aba5fb19751c00b24f1a9d
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-30 11:47:31 +0100

    load font

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import Engine from "./Globals/Engine"
 import Run from "./GameStates/RunState"
 import LevelUp from "./GameStates/LevelUpState"
+import m5xy from '../assets/fonts/m5x7.ttf'
 //@ts-ignore
 import { preloadFont } from 'troika-three-text'
 
 await new Promise<void>((resolve) => preloadFont(
-	{ font: '../assets/fonts/m5x7.ttf', }, () => resolve()
+	{ font: m5xy, }, () => resolve()
 ))
 Engine.addState('run', new Run())
 Engine.addState('levelUp', new LevelUp())
