aa0381067915193e6640c9c4b52ea8076c07c66c
Vincent Douchin <vincent.douchin@gmail.com>
2022-12-30 09:22:54 +0100

    fixed xp bar

--- src/Utils/UpdateBar.ts
+++ src/Utils/UpdateBar.ts
@@ -1,10 +1,10 @@
 import MeshComponent from '../Components/MeshComponent'
 import Tile from './Tile'
 const updateBar = (mesh: MeshComponent, from: Tile, to: Tile, percent: number) => {
-	const fullWidth = Math.ceil(percent * mesh.width)
 	const image: HTMLCanvasElement = mesh.texture.image
+	const fullWidth = Math.ceil(percent * mesh.texture.image.width)
 	const ctx = image.getContext('2d')
-	ctx!.drawImage(from.buffer.canvas, 0, 0)
+	ctx!.drawImage(from.buffer.canvas, 0, 0,)
 	ctx!.drawImage(to.buffer.canvas, 0, 0, fullWidth, mesh.height, 0, 0, fullWidth, mesh.height)
 	mesh.texture.needsUpdate = true
 }
