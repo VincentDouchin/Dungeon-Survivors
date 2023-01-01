39ae03454a837da9be7b6378cdb722ad893735a2
Vincent Douchin <vincent.douchin@gmail.com>
2023-01-01 16:22:04 +0100

    removed global state

--- src/UIEntities/LevelDisplayEntity.ts
+++ src/UIEntities/LevelDisplayEntity.ts
@@ -4,14 +4,13 @@ import UIPosition from "../Components/UIPosition"
 import ECSEVENTS from "../Constants/ECSEvents"
 import AssetManager from "../Globals/AssetManager"
 import { ECS, Entity } from "../Globals/ECS"
-import State from "../Globals/State"
 
 const LevelDisplayEntity = () => {
 	const level = new Entity()
 	// debugger
 	level.addComponent(new UIPosition({ x: -1, y: -1 }, { x: -1, y: -1 }))
 	level.addComponent(new MeshComponent(AssetManager.UI.box, { scale: 1.5 }))
-	const text = level.addComponent(new TextComponent(String(State.level), { size: 32 }))
+	const text = level.addComponent(new TextComponent(String(0), { size: 32 }))
 	ECS.eventBus.subscribe(ECSEVENTS.LEVELUP, (level: number) => {
 		text.setText(String(level))
 	})
