5c554e5317a4be4c65f127fd95b6a5a36ccbba4a
Vincent Douchin <vincent.douchin@gmail.com>
2023-01-05 07:14:32 +0100

    added options for anchor and width

--- src/Components/TextComponent.ts
+++ src/Components/TextComponent.ts
@@ -5,20 +5,20 @@ class TextComponent extends Component {
 	previousText: string = ''
 	text: string
 	mesh: Text
-	constructor(text: string, options?: { size?: number, color?: number }) {
+	constructor(text: string, options?: { size?: number, color?: number, maxWidth?: number, anchorY?: string, anchorX?: string }) {
 		super()
-		const newOptions = Object.assign({ size: 16, color: 0xffffff }, options)
+		const newOptions = Object.assign({ size: 16, color: 0xffffff, anchorX: 'center', anchorY: 'middle' }, options)
 		this.text = text
 		this.previousText = text
 		this.mesh = new Text()
 		this.mesh.text = text
 		this.mesh.fontSize = newOptions.size
 		this.mesh.font = '../../assets/fonts/m5x7.ttf'
-		this.mesh.anchorY = 'middle'
-		this.mesh.anchorX = 'center'
+		this.mesh.anchorY = newOptions.anchorY
+		this.mesh.anchorX = newOptions.anchorX
 		this.mesh.color = newOptions.color
 		this.mesh.renderOrder = 10
-
+		this.mesh.maxWidth = 10
 
 	}
 	setText(newText: string) {
