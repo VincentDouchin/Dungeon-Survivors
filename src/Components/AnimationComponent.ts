4a70773908c6d113c0d2c429eeb45ec16b87f59b
Vincent Douchin <vincent.douchin@gmail.com>
2023-01-05 07:14:11 +0100

    added playAnimation

--- src/Components/AnimationComponent.ts
+++ src/Components/AnimationComponent.ts
@@ -1,3 +1,4 @@
+import Coroutines from "../Globals/Coroutines";
 import { Component } from "../Globals/ECS";
 import Tile from "../Utils/Tile";
 
@@ -6,13 +7,17 @@ class AnimationComponent extends Component {
 	lastState: string = ''
 	currentState: string = ''
 	selectedFrame = 0
-	frameRate = 10
+	frameRate: number
 	frameCounter = 0
 	maxFrames: number
 	flipped = false
-	constructor(tiles: { [key: string]: Tile }) {
+	start: boolean
+	constructor(tiles: { [key: string]: Tile }, options?: { start?: boolean, frameRate?: number }) {
 		super()
+		const newOptions = Object.assign({ start: true, frameRate: 10 }, options)
 		this.tiles = tiles
+		this.frameRate = newOptions.frameRate
+		this.start = newOptions.start
 		this.maxFrames = Math.max(...Object.values(tiles).map(tile => tile.frames))
 		this.currentState = Object.keys(tiles)[0]
 		this.lastState = this.currentState
@@ -27,6 +32,24 @@ class AnimationComponent extends Component {
 
 	get tile() {
 		return this.tiles[this.currentState]
+	}
+	playAnimation(animationsName?: string) {
+		this.start = true
+		const animation = animationsName ?? Object.keys(this.tiles)[0]
+		this.currentState = animation
+		const self = this
+		return new Promise<void>(resolve => {
+			Coroutines.add(function* () {
+				while (self.selectedFrame < self.tiles[animation].frames - 1) {
+					console.log(self.selectedFrame, self.tiles[animation].frames)
+					yield
+				}
+				console.log('done')
+				resolve()
+				return
+			})
+		})
+
 	}
 }
 AnimationComponent.register()
--- src/Systems/AnimationSystem.ts
+++ src/Systems/AnimationSystem.ts
@@ -12,21 +12,24 @@ class AnimationSystem extends System {
 			const animation = entity.getComponent(AnimationComponent)
 			const mesh = entity.getComponent(MeshComponent)
 			animation.frameCounter++
-			if (animation.frameCounter > animation.frameRate) {
-				animation.frameCounter = 0
-				animation.selectedFrame = (animation.selectedFrame + 1) % animation.frames
+			if (animation.start) {
+				if (animation.frameCounter > animation.frameRate) {
+					animation.frameCounter = 0
+					animation.selectedFrame = (animation.selectedFrame + 1) % animation.frames
+				}
+
+
+				if (animation.currentState != animation.lastState || mesh.modifier != mesh.lastModifer) {
+
+					mesh.texture.image = animation.tile[mesh.modifier]!.canvas
+
+					mesh.lastModifer = mesh.modifier
+					mesh.texture.needsUpdate = true
+					animation.selectedFrame = 0
+				}
 			}
 			mesh.texture.repeat.x = (animation.flipped ? -1 : 1) / animation.maxFrames
 			mesh.texture.offset.x = ((animation.flipped ? 1 : 0) + animation.selectedFrame) / animation.frames
-
-			if (animation.currentState != animation.lastState || mesh.modifier != mesh.lastModifer) {
-
-				mesh.texture.image = animation.tile[mesh.modifier]!.canvas
-
-				mesh.lastModifer = mesh.modifier
-				mesh.texture.needsUpdate = true
-				animation.selectedFrame = 0
-			}
 		})
 	}
 }
