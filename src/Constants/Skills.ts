e96cef9b7afc6602af2fcdacddfbe1ee9938b8b3
Vincent Douchin <vincent.douchin@gmail.com>
2023-01-06 07:47:19 +0100

    added crit damage modifier

--- src/Constants/Skills.ts
+++ src/Constants/Skills.ts
@@ -1,40 +1,41 @@
+import SkillsComponent from "../Components/SkillsComponent"
 import AssetManager from "../Globals/AssetManager"
 
 const SKILLS: Skill[] = [
 	{
 		icon: AssetManager.icons.turnSpeed,
 		name: 'Rotation speed',
-		modifier: (skill) => skill.angVel += 1
+		modifier: (skill: SkillsComponent) => skill.angVel += 1
 	},
 	{
 		icon: AssetManager.icons.damage,
 		name: 'Damage',
-		modifier: (skill) => skill.damage += 1
+		modifier: (skill: SkillsComponent) => skill.damage += 1
 	},
 	{
 		icon: AssetManager.icons.critdamage,
 		name: 'Critical damage',
-		modifier: (skill) => skill.angVel += 1
+		modifier: (skill: SkillsComponent) => skill.critDamage *= 1.5
 	},
 	{
 		icon: AssetManager.icons.poison,
 		name: 'Poison',
-		modifier: (skill) => skill.angVel += 1
+		modifier: (skill: SkillsComponent) => skill.angVel += 1
 	},
 	{
 		icon: AssetManager.icons.curse,
 		name: 'Curse',
-		modifier: (skill) => skill.angVel += 1
+		modifier: (skill: SkillsComponent) => skill.angVel += 1
 	},
 	{
 		icon: AssetManager.icons.bleed,
 		name: 'Bleed',
-		modifier: (skill) => skill.angVel += 1
+		modifier: (skill: SkillsComponent) => skill.angVel += 1
 	},
 	{
 		icon: AssetManager.icons.slow,
 		name: 'Slow',
-		modifier: (skill) => skill.angVel += 1
+		modifier: (skill: SkillsComponent) => skill.angVel += 1
 	},
 ]
 export default SKILLS
