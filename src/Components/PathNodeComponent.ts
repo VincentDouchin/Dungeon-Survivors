1b985ac79baca05696833dd488b75d39de328a60
Vincent Douchin <vincent.douchin@gmail.com>
2023-01-12 09:51:47 +0100

    start run state if path node is encounter

--- src/Components/PathNodeComponent.ts
+++ src/Components/PathNodeComponent.ts
@@ -4,9 +4,11 @@ class PathNodeComponent extends Component {
 	nodes: Partial<Record<nodeDirection, Entity>>
 	selected: boolean
 	walker: Entity
+	encounter: boolean
 	showingOptions = false
-	constructor(nodes: Partial<Record<nodeDirection, Entity>>, walker: Entity, selected: boolean) {
+	constructor(nodes: Partial<Record<nodeDirection, Entity>>, walker: Entity, selected: boolean, encounter: boolean) {
 		super()
+		this.encounter = encounter
 		this.nodes = nodes
 		this.walker = walker
 		this.selected = selected
