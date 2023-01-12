787bfb1043f7fb31ed00924cc2d0f39bef94a0b6
Vincent Douchin <vincent.douchin@gmail.com>
2023-01-12 09:55:31 +0100

    change name of entity

--- src/UIEntities/UIRunEntity.ts
+++ src/UIEntities/UIRunEntity.ts
@@ -2,10 +2,10 @@
 import { Entity } from "../Globals/ECS"
 import XPBarEntity from "./XPBarEntity"
 import LevelDisplayEntity from "./LevelDisplayEntity"
-const RunUIEntity = () => {
+const UIRunEntity = () => {
 	const ui = new Entity()
 	const level = ui.addChildren(LevelDisplayEntity())
 	level.addChildren(XPBarEntity())
 	return ui
 }
-export default RunUIEntity
+export default UIRunEntity
