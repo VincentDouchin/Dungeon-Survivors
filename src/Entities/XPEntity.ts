cd725138d92ed8366074c6c548591c3919029c82
Vincent Douchin <vincent.douchin@gmail.com>
2023-01-22 00:39:19 +0100

    reduce speed

--- src/Entities/XPEntity.ts
+++ src/Entities/XPEntity.ts
@@ -1,15 +1,15 @@
 import BodyComponent from "../Components/BodyComponent"
-import SpriteComponent from "../Components/SpriteComponent"
-import XPComponent from "../Components/XPComponent"
 import COLLISIONGROUPS from "../Constants/CollisionGroups"
 import { Entity } from "../Globals/ECS"
+import SpriteComponent from "../Components/SpriteComponent"
+import XPComponent from "../Components/XPComponent"
 import { assets } from "../Globals/Initialize"
 
 const XPEntity = () => {
 	const xp = new Entity()
 	xp.addComponent(new SpriteComponent(assets.tiles.xp, { renderOrder: 1, scale: 0.5 }))
 	xp.addComponent(new BodyComponent(
-		{ moveForce: 10000 },
+		{ moveForce: 1000 },
 		[
 			{ width: 2, height: 2, contact: true, group: COLLISIONGROUPS.XP, canCollideWith: [COLLISIONGROUPS.SENSOR], mass: 1 }
 		]))
