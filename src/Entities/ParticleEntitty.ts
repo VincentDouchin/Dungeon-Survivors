365aaa1fd044587048d2df0827712b40a2a28e9f
Vincent Douchin <vincent.douchin@gmail.com>
2023-01-22 14:14:27 +0100

    change particles to use any tile

--- src/Entities/ParticleEntitty.ts
+++ src/Entities/ParticleEntitty.ts
@@ -1,15 +1,14 @@
 import AnimationComponent from "../Components/AnimationComponent"
-import SpriteComponent from "../Components/SpriteComponent"
-import PositionComponent from "../Components/PositionComponent"
 import { Entity } from "../Globals/ECS"
-import { assets } from "../Globals/Initialize"
+import PositionComponent from "../Components/PositionComponent"
+import SpriteComponent from "../Components/SpriteComponent"
+import Tile from "../Utils/Tile"
 
-const ParticleEntity = async (x: number, y: number, animation?: string) => {
-	const tile = assets.magic[animation ?? 'smoke']
+const ParticleEntity = async (x: number, y: number, tile: Tile, frameRate?: number) => {
 	const particle = new Entity()
 	particle.addComponent(new SpriteComponent(tile))
 	particle.addComponent(new PositionComponent(x, y))
-	const testAnimation = particle.addComponent(new AnimationComponent({ default: tile }, { start: false, frameRate: 5 }))
+	const testAnimation = particle.addComponent(new AnimationComponent({ default: tile }, { start: false, frameRate: frameRate ?? 5 }))
 	return testAnimation.playAnimation().then(() => particle.destroy())
 }
 export default ParticleEntity
