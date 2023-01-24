ce4c6db1c2937d4a496b0a35069eb82bb9e67969
Vincent Douchin <vincent.douchin@gmail.com>
2023-01-24 12:58:21 +0100

    use bodySizes to create bodies

---
+++ src/Constants/BodySizes.ts
@@ -0,0 +1,39 @@
+export interface bodySize {
+	width: number
+	height: number
+}
+const BODYSIZES: Record<string, bodySize> = {
+	small: {
+		width: 10,
+		height: 10
+	},
+	normal: {
+		width: 12,
+		height: 19
+	},
+	big: {
+		width: 18,
+		height: 30
+	},
+	square: {
+		width: 16,
+		height: 16
+	},
+	centaur: {
+		width: 18,
+		height: 22
+	},
+	massive: {
+		width: 30,
+		height: 22
+	},
+	giant: {
+		width: 32,
+		height: 32
+	},
+	wide: {
+		width: 22,
+		height: 16
+	}
+}
+export default BODYSIZES
--- src/Entities/EnemyEntity.ts
+++ src/Entities/EnemyEntity.ts
@@ -3,6 +3,7 @@ import BodyComponent from "../Components/BodyComponent"
 import COLLISIONGROUPS from "../Constants/CollisionGroups"
 import DamageComponent from "../Components/DamageComponent"
 import DroppableComponent from "../Components/DroppableComponent"
+import { EnemyType } from "../Constants/Enemies"
 import { Entity } from "../Globals/ECS"
 import HealthComponent from "../Components/HealthComponent"
 import PositionComponent from "../Components/PositionComponent"
@@ -13,19 +14,19 @@ import XPEntity from "./XPEntity"
 
 const EnemyEntity = (type: EnemyType, position: { x: number, y: number }) => {
 	const enemy = new Entity()
-	const orc = Object.values(type.tiles)[0]
-	enemy.addComponent(new SpriteComponent(orc))
+	const tile = Object.values(type.tiles)[0]
+	enemy.addComponent(new SpriteComponent(tile))
 	enemy.addComponent(new AnimationComponent(type.tiles))
 	enemy.addComponent(new DamageComponent(1, [COLLISIONGROUPS.PLAYER], -1, 2))
 	enemy.addComponent(new HealthComponent(type.health, COLLISIONGROUPS.ENEMY))
 	enemy.addComponent(new DroppableComponent(XPEntity))
 	enemy.addComponent(new PositionComponent(position.x, position.y))
 	enemy.addComponent(new TargeterComponent(COLLISIONGROUPS.PLAYER))
-	enemy.addComponent(new ShadowComponent(orc.width, 6, orc.height / 2))
+	enemy.addComponent(new ShadowComponent(type.size.width, 6, tile.height / 2))
 	enemy.addComponent(new BodyComponent(
-		{ moveForce: 100 },
+		{ moveForce: 30 },
 		[
-			{ width: orc.width, height: orc.height, contact: false, group: COLLISIONGROUPS.ENEMY, canCollideWith: [COLLISIONGROUPS.ENEMY, COLLISIONGROUPS.PLAYER, COLLISIONGROUPS.TRAP, COLLISIONGROUPS.WEAPON, COLLISIONGROUPS.WALL] }
+			{ width: type.size.width, height: type.size.height, offset: tile.height, contact: false, group: COLLISIONGROUPS.ENEMY, canCollideWith: [COLLISIONGROUPS.ENEMY, COLLISIONGROUPS.PLAYER, COLLISIONGROUPS.TRAP, COLLISIONGROUPS.WEAPON, COLLISIONGROUPS.WALL] }
 		]
 	))
 	return enemy
