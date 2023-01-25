8115db2129721316710506f2f36229bfbf77d75c
Vincent Douchin <vincent.douchin@gmail.com>
2023-01-25 14:10:02 +0100

    move sources

--- src/Utils/LDTKMap.ts
+++ src/Utils/LDTKMap.ts
@@ -15,7 +15,7 @@ interface entityInstanceFormatted {
 	height: number
 }
 export type ldtkNode = entityInstanceFormatted & node
-const sources: Record<string, { default: string }> = import.meta.glob('/assets/**/_composite.png', { eager: true })
+
 class LDTKMap implements LDTKMapDefinition {
 	static tiles: Record<string, Tile> = {}
 	constructor(data: LDTKMap) {
@@ -25,6 +25,7 @@ class LDTKMap implements LDTKMapDefinition {
 
 	static async load(data: any) {
 		for (let level of data.levels) {
+			const sources: Record<string, { default: string }> = import.meta.glob('/assets/**/_composite.png', { eager: true })
 			const source = Object.entries(sources).reduce<string>((acc, [path, source]) => {
 				if (path.split('/').at(-2) == level.identifier) return source.default
 				return acc
