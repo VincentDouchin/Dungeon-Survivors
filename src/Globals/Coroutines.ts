2328d71c8ea91f2b532bfc906df0a5eb12b48b0a
Vincent Douchin <vincent.douchin@gmail.com>
2023-01-25 13:36:43 +0100

    change coroutine to an object

--- src/Globals/Coroutines.ts
+++ src/Globals/Coroutines.ts
@@ -2,15 +2,22 @@ interface Coroutine {
 	generator: Generator
 	state: 'running' | 'stopped'
 }
-const Coroutines = new class {
-	coroutines: Coroutine[] = []
+const Coroutines: {
+	coroutines: Coroutine[],
+	add: (fn: () => Generator) => void,
+	run: () => void,
+	stop: () => void,
+	resume: () => void,
+} = {
+
+	coroutines: [],
 	add(fn: () => Generator) {
 		const generator = fn()
 		generator.next()
 		const coroutine: Coroutine = { generator, state: 'running' }
 		this.coroutines.push(coroutine)
 		return coroutine
-	}
+	},
 	run() {
 		for (let i = this.coroutines.length - 1; i >= 0; i--) {
 			if (this.coroutines[i].state == 'stopped') return
@@ -19,12 +26,12 @@ const Coroutines = new class {
 				this.coroutines.splice(i, 1)
 			}
 		}
-	}
+	},
 	stop() {
 		this.coroutines.forEach(coroutine => {
 			coroutine.state = 'stopped'
 		})
-	}
+	},
 	resume() {
 		this.coroutines.forEach(coroutine => {
 			coroutine.state = 'running'
