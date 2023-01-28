7cf28585b32757b5c74172a8d985ee4530bd2bab
Vincent Douchin <vincent.douchin@gmail.com>
2023-01-28 11:10:07 +0100

    change type of event bus methods

--- src/UIEntities/XPBarEntity.ts
+++ src/UIEntities/XPBarEntity.ts
@@ -1,10 +1,12 @@
+import { ECS, Entity } from "../Globals/ECS"
+import ECSEVENTS, { XP_PERCENT } from "../Constants/ECSEvents"
+
+import BarShader from "../Shaders/BarShader"
 import SpriteComponent from "../Components/SpriteComponent"
 import UIPosition from "../Components/UIPosition"
-import ECSEVENTS from "../Constants/ECSEvents"
-import { ECS, Entity } from "../Globals/ECS"
 import { assets } from "../Globals/Initialize"
-import BarShader from "../Shaders/BarShader"
 import framedTile from "../Utils/FramedTile"
+
 const scalingOptions = { x: { left: 2, right: 4 }, y: { top: 0, bottom: 0 } }
 const w = 100
 const h = 7
@@ -13,7 +15,7 @@ const full = framedTile(assets.UI.XPFull, scalingOptions, w, h)
 const XPBarEntity = () => {
 	const xpBar = new Entity()
 	const sprite = xpBar.addComponent(new SpriteComponent(bar, { renderOrder: 100, scale: 3, shaders: [new BarShader(full.texture, 0)] }))
-	ECS.eventBus.subscribe(ECSEVENTS.XPPERCENT, (percent: number) => {
+	ECS.eventBus.subscribe<XP_PERCENT>(ECSEVENTS.XP_PERCENT, (percent: number) => {
 		sprite.uniforms.percent = percent
 	})
 	xpBar.addComponent(new UIPosition({ x: 1, y: 1 }, { x: -1, y: 1 }))
