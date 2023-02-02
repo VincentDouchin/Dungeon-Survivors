8519277116e2fc99d0c8ed013de957654d7b673c
Vincent Douchin <vincent.douchin@gmail.com>
2023-02-02 16:46:27 +0100

    bersek and weapon

--- src/Entities/EnemyEntity.ts
+++ src/Entities/EnemyEntity.ts
@@ -6,29 +6,38 @@ import DroppableComponent from "../Components/DroppableComponent"
 import { EnemyType } from "../Constants/Enemies"
 import { Entity } from "../Globals/ECS"
 import HealthComponent from "../Components/HealthComponent"
+import OutlineShader from "../Shaders/OutlineShader"
 import PositionComponent from "../Components/PositionComponent"
 import ShadowComponent from "../Components/ShadowComponent"
 import SpriteComponent from "../Components/SpriteComponent"
 import TargeterComponent from "../Components/TargeterComponent"
+import WeaponEntity from "./WeaponEntity"
 import XPEntity from "./XPEntity"
 
 const EnemyEntity = (type: EnemyType, position: { x: number, y: number }) => {
 	const enemy = new Entity('enemy')
+	const berserk = type.berserk
 	const tile = Object.values(type.tiles)[0]
-	enemy.addComponent(new SpriteComponent(tile))
+	const sprite = enemy.addComponent(new SpriteComponent(tile))
+	if (berserk) {
+		sprite.addShader(new OutlineShader([1, 0, 0, 1]))
+	}
 	enemy.addComponent(new AnimationComponent(type.tiles))
-	enemy.addComponent(new DamageComponent(1, [COLLISIONGROUPS.PLAYER], -1, 2))
-	enemy.addComponent(new HealthComponent(type.health, COLLISIONGROUPS.ENEMY))
+	enemy.addComponent(new DamageComponent((type.damage ?? 1), [COLLISIONGROUPS.PLAYER], -1, 2))
+	enemy.addComponent(new HealthComponent(type.health * (berserk ? 1.5 : 1), COLLISIONGROUPS.ENEMY))
 	enemy.addComponent(new DroppableComponent(XPEntity))
 	enemy.addComponent(new PositionComponent(position.x, position.y))
 	enemy.addComponent(new TargeterComponent(COLLISIONGROUPS.PLAYER))
 	enemy.addComponent(new ShadowComponent(type.size.width, 6, tile.height / 2))
 	enemy.addComponent(new BodyComponent(
-		{ moveForce: 300 * type.speed },
+		{ moveForce: 300 * type.speed * (berserk ? 1.5 : 1) },
 		[
 			{ width: type.size.width, height: type.size.height, mass: 1, offset: tile.height, contact: false, group: COLLISIONGROUPS.ENEMY, canCollideWith: [COLLISIONGROUPS.ENEMY, COLLISIONGROUPS.PLAYER, COLLISIONGROUPS.TRAP, COLLISIONGROUPS.WEAPON, COLLISIONGROUPS.WALL] }
 		]
 	))
+	if (type.weapon) {
+		enemy.addChildren(WeaponEntity(type.weapon, enemy))
+	}
 	return enemy
 }
 
