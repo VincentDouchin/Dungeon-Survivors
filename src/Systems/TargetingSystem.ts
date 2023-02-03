e358e0fbbd610706d833f5985336e0240be304a1
Vincent Douchin <vincent.douchin@gmail.com>
2023-02-03 16:41:29 +0100

    change back targeting

--- src/Systems/TargetingSystem.ts
+++ src/Systems/TargetingSystem.ts
@@ -21,18 +21,19 @@ class TargetingSystem extends System {
 			const joint = entity.getComponent(JointComponent)
 			const body = entity.getComponent(BodyComponent)
 			const ranged = entity.getComponent(RangedComponent)
-
-			const enemyId = ECS.getEntitiesAndComponents(HealthComponent)
-				.reduce<[string, number]>(([target, distance], [entityId, health]) => {
-					if (health.type == targeter.target) {
-						const enemyPosition = ECS.getEntityById(entityId).getComponent(PositionComponent)
-						const distanceToEnemy = enemyPosition.distanceTo(position)
-						if (distanceToEnemy < distance) return [entityId, distanceToEnemy]
-						if (distance == 0) return [entityId, distanceToEnemy]
-					}
-					return [target, distance]
-				}, ['', 0])[0]
-			targeter.targetedEnemy = enemyId
+			if (!targeter.targetedEnemy) {
+				const enemyId = ECS.getEntitiesAndComponents(HealthComponent)
+					.reduce<[string, number]>(([target, distance], [entityId, health]) => {
+						if (health.type == targeter.target) {
+							const enemyPosition = ECS.getEntityById(entityId).getComponent(PositionComponent)
+							const distanceToEnemy = enemyPosition.distanceTo(position)
+							if (distanceToEnemy < distance) return [entityId, distanceToEnemy]
+							if (distance == 0) return [entityId, distanceToEnemy]
+						}
+						return [target, distance]
+					}, ['', 0])[0]
+				targeter.targetedEnemy = enemyId
+			}
 
 			const rotation = entity.getComponent(RotationComponent)
 			if (targeter.targetedEnemy) {
