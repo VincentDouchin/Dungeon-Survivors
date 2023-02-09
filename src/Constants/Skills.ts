9549e0ce2df9a7e27061750177d0729e85315660
Vincent Douchin <vincent.douchin@gmail.com>
2023-02-09 18:52:52 +0100

    update skills

--- src/Constants/Skills.ts
+++ src/Constants/Skills.ts
@@ -5,37 +5,37 @@ const SKILLS: Skill[] = [
 	{
 		icon: assets.skills.attack_speed_boost,
 		name: 'Attack speed',
-		modifier: (skill: StatsComponent) => skill.attackSpeed.addPercent(0.2)
+		modifier: (skill: StatsComponent) => skill.attackSpeed.percent += 0.2
 	},
 	{
 		icon: assets.skills.attack_boost,
 		name: 'Damage',
-		modifier: (skill: StatsComponent) => skill.damage.addPercent(0.1)
+		modifier: (skill: StatsComponent) => skill.damage.percent += 0.1
 	},
 	{
 		icon: assets.skills.critical_boost,
 		name: 'Critical damage',
-		modifier: (skill: StatsComponent) => skill.critDamage.addPercent(0.5)
+		modifier: (skill: StatsComponent) => skill.critDamage.percent += 0.3
 	},
 	{
 		icon: assets.skills.defense_boost,
 		name: 'Defense',
-		modifier: (skill: StatsComponent) => skill.defense.addPercent(0.2)
+		modifier: (skill: StatsComponent) => skill.defense.percent += 0.1
 	},
 	{
 		icon: assets.skills.exp_boost,
 		name: 'Experience up',
-		modifier: (skill: StatsComponent) => skill.xpModifier.addFlat(10)
+		modifier: (skill: StatsComponent) => skill.xpModifier.percent += 0.05
 	},
 	{
 		icon: assets.skills.knockback_boost,
 		name: 'Knockback',
-		modifier: (skill: StatsComponent) => skill.knockback.addPercent(0.2)
+		modifier: (skill: StatsComponent) => skill.knockback.percent += 0.2
 	},
 	{
 		icon: assets.skills.swiftness,
 		name: 'Movement speed',
-		modifier: (skill: StatsComponent) => skill.speed.addPercent(0.2)
+		modifier: (skill: StatsComponent) => skill.speed.percent += 0.15
 	},
 
 ]
