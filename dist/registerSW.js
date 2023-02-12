013069af76f3af5c2122beb6421c52f5733198bd
Vincent Douchin <vincent.douchin@gmail.com>
2023-02-12 09:26:50 +0100

    build

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Dungeon-Survivor/custom-sw.ts', { scope: '/Dungeon-Survivor/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Dungeon-Survivor/sw.js', { scope: '/Dungeon-Survivor/' })})}
