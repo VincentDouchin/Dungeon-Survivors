0c65d47be4f14cc10845bbe16552aabc50ed00e5
Vincent Douchin <vincent.douchin@gmail.com>
2023-02-16 16:53:53 +0100

    remove vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,8 @@
-import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
-		VitePWA({
-			injectManifest: {
-				injectionPoint: undefined,
-			},
-			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'favicon.svg'],
-			manifest: {
-				name: 'Dungeon Survivor',
-				short_name: 'DungeonSurvivor',
-				description: 'Dungeon Survivor',
-				theme_color: '#000000',
-				orientation: 'landscape',
-				icons: [
-					{
-						src: 'pwa-192x192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: 'pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png'
-					},
-					{
-						src: 'pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png',
-						purpose: 'any maskable'
-					}
-				],
 
-			}
-
-		}),
 	],
 	base: "/Dungeon-Survivor/",
 	build: {
