59cbc0d55108b1dfe8d380512eced270c21eebc8
Vincent Douchin <vincent.douchin@gmail.com>
2023-02-16 17:11:37 +0100

    change path for icons

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 		VitePWA({
 			registerType: 'autoUpdate',
 			injectRegister: 'auto',
-			includeAssets: ['/favicon.ico', '/apple-touch-icon.png'],
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
 			manifest: {
 				name: 'Dungeon Survivor',
 				short_name: 'DungeonSurvivor',
@@ -14,12 +14,12 @@ export default defineConfig({
 				theme_color: '#000000',
 				icons: [
 					{
-						src: '/pwa-192x192.png',
+						src: 'pwa-192x192.png',
 						sizes: '192x192',
 						type: 'image/png'
 					},
 					{
-						src: '/pwa-512x512.png',
+						src: 'pwa-512x512.png',
 						sizes: '512x512',
 						type: 'image/png'
 					}
