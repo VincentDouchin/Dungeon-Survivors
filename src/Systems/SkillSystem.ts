f2d535e3e2b5ae692af5d17c1e545f8dd9a80132
Vincent Douchin <vincent.douchin@gmail.com>
2023-02-19 18:37:59 +0100

    select main entity for skill system

--- src/Systems/SkillSystem.ts
+++ src/Systems/SkillSystem.ts
@@ -4,6 +4,7 @@ import DivineProtectionEntity from "../Entities/DivineProtectionEntity";
 import PositionComponent from "../Components/PositionComponent";
 import { SKILL } from "../Constants/InputsNames";
 import StatsComponent from "../Components/StatsComponent";
+import SwitchingComponent from "../Components/SwitchingComponent";
 import { inputManager } from "../Globals/Initialize";
 
 class SkillSystem extends System {
@@ -11,13 +12,15 @@ class SkillSystem extends System {
 		super(StatsComponent)
 	}
 	update(entities: Entity[]) {
-		entities.forEach(entity => {
-			if (inputManager.getInput(SKILL)?.once) {
-				const position = entity.getComponent(PositionComponent)
+		entities
+			.filter(entity => entity.getComponent(SwitchingComponent)?.main)
+			.forEach(entity => {
+				if (inputManager.getInput(SKILL)?.once) {
+					const position = entity.getComponent(PositionComponent)
 
-				DivineProtectionEntity(position.position)
-			}
-		})
+					DivineProtectionEntity(position.position)
+				}
+			})
 	}
 }
 export default SkillSystem
