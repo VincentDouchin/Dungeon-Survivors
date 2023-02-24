16e2c15d534b2e18747772193fadc0e8a889d5de
Vincent Douchin <vincent.douchin@gmail.com>
2023-02-24 18:49:19 +0100

    change size of bars

--- src/UIEntities/ManaBarEntity.ts
+++ src/UIEntities/ManaBarEntity.ts
@@ -8,17 +8,17 @@ import assets from "../Globals/Assets"
 import framedTile from "../Utils/FramedTile"
 
 const scalingOptions = { x: { left: 2, right: 4 }, y: { top: 0, bottom: 0 } }
-const w = 40
+const w = window.innerWidth * 0.05
 const h = 7
 const bar = framedTile(assets.UI.bar, scalingOptions, w, h)
 const full = framedTile(assets.UI.mana, scalingOptions, w, h)
 const ManaBarEntity = () => {
 	const manaBar = new Entity('mana bar')
-	const sprite = manaBar.addComponent(new SpriteComponent(bar, { renderOrder: 100, scale: 3, shaders: [new BarShader(full.texture, 0)] }))
+	const sprite = manaBar.addComponent(new SpriteComponent(bar, { renderOrder: 100, scale: 3, shaders: [new BarShader(full.texture, 0)], flipped: true }))
 	ECS.eventBus.subscribe<MANA_PERCENT>(ECSEVENTS.MANA_PERCENT, (percent: number) => {
 		sprite.uniforms.percent = percent
 	})
-	manaBar.addComponent(new UIPositionComponent({ x: -1, y: -1 }, { x: -1, y: 1 }))
+	manaBar.addComponent(new UIPositionComponent({ x: -1, y: 1 }, { x: 1, y: 1 }))
 	return manaBar
 }
 export default ManaBarEntity
