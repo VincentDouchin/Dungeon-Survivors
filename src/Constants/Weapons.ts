25f94b48c08248225cdcf968cfe52a66f98cdcd1
Vincent Douchin <vincent.douchin@gmail.com>
2023-02-25 17:24:59 +0100

    clean up weapons and add hammer

--- src/Constants/Weapons.ts
+++ src/Constants/Weapons.ts
@@ -19,128 +19,9 @@ export interface WeaponDefinition {
 	light?: string
 }
 const WEAPONS: Record<string, WeaponDefinition> = {
-	knife: {
-		tile: assets.tiles.weapon_knife,
-		damage: 5,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	swordOld: {
-		tile: assets.tiles.weapon_rusty_sword,
-		damage: 7,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	sword: {
-		tile: assets.tiles.weapon_regular_sword,
-		damage: 10,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	swordGem: {
-		tile: assets.tiles.weapon_red_gem_sword,
-		damage: 15,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	hammerLong: {
-		tile: assets.tiles.weapon_big_hammer,
-		damage: 15,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	hammer: {
-		tile: assets.tiles.weapon_hammer,
-		damage: 15,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	club: {
-		tile: assets.tiles.weapon_baton_with_spikes,
-		damage: 10,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	mace: {
-		tile: assets.tiles.weapon_mace,
-		damage: 15,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	katana: {
-		tile: assets.tiles.weapon_katana,
-		damage: 5,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	swordSaw: {
-		tile: assets.tiles.weapon_saw_sword,
-		damage: 10,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	swordAnime: {
-		tile: assets.tiles.weapon_anime_sword,
-		damage: 20,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	hatchet: {
-		tile: assets.tiles.weapon_axe,
-		damage: 10,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	machete: {
-		tile: assets.tiles.weapon_machete,
-		damage: 15,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	cleaver: {
-		tile: assets.tiles.weapon_cleaver,
-		damage: 15,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	rapier: {
-		tile: assets.tiles.weapon_duel_sword,
-		damage: 10,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
 	swordKnight: {
 		tile: assets.tiles.weapon_knight_sword,
 		damage: 10,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	swordGolden: {
-		tile: assets.tiles.weapon_golden_sword,
-		damage: 15,
-		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
-		group: COLLISIONGROUPS.PLAYER,
-		target: COLLISIONGROUPS.ENEMY
-	},
-	swordGoldenBig: {
-		tile: assets.tiles.weapon_lavish_sword,
-		damage: 15,
 		behaviors: [WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.toucher],
 		group: COLLISIONGROUPS.PLAYER,
 		target: COLLISIONGROUPS.ENEMY
@@ -209,6 +90,18 @@ const WEAPONS: Record<string, WeaponDefinition> = {
 		range: 200,
 		rotationSpeed: 1,
 		speed: 200
+	},
+	hammer: {
+		tile: assets.UI.empty,
+		damage: 5,
+		behaviors: [WEAPONBEHAVIORS.targeter, WEAPONBEHAVIORS.orbiter, WEAPONBEHAVIORS.shooter],
+		projectile: assets.tiles.weapon_hammer_projectile,
+		group: COLLISIONGROUPS.ENEMY,
+		target: COLLISIONGROUPS.PLAYER,
+		delay: 240,
+		range: 150,
+		rotationSpeed: 0.5,
+		speed: 150
 	}
 }
 export default WEAPONS
