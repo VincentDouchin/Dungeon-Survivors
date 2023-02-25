4e01facc6d0f65dae1c75900c601e524ef7f80c8
Vincent Douchin <vincent.douchin@gmail.com>
2023-02-25 17:24:41 +0100

    adjust encounters

--- src/Constants/EnemyEncounters.ts
+++ src/Constants/EnemyEncounters.ts
@@ -6,8 +6,8 @@ export type enemyWaveName = 'ORCS' | 'ANIMALS' | 'PRIESTS' | 'ELVES' | 'DEMONS' 
 const berserk = (enemyType: EnemyType): EnemyType => ({ ...enemyType, berserk: true })
 
 
-const ENENMYENCOUNTERS: Partial<Record<enemyWaveName, Encounter>> = {
-	ORCS: new Encounter()
+const ENENMYENCOUNTERS: Partial<Record<enemyWaveName, () => Encounter>> = {
+	ORCS: () => new Encounter()
 		.setBoundary(800, 800)
 		.addWave([[Enemies.goblin, 20], [berserk(Enemies.goblin), 5]], 5)
 		.addWave([[berserk(Enemies.orc), 2], [Enemies.orc, 5], [Enemies.goblin, 15], [berserk(Enemies.goblin), 5]], 5)
@@ -19,7 +19,7 @@ const ENENMYENCOUNTERS: Partial<Record<enemyWaveName, Encounter>> = {
 		.waitForEnemiesCleared()
 		.stop()
 	,
-	ANIMALS: new Encounter()
+	ANIMALS: () => new Encounter()
 		.addWave([[berserk(Enemies.bunny), 5], [Enemies.bunny, 10], [Enemies.mushroomSmall, 10], [Enemies.mushroomMedium, 10]], 5)
 		.addWave([[Enemies.direwolf, 10], [Enemies.bunny, 10]], 5)
 		.addWave([[Enemies.centaurFemale, 5], [Enemies.centaurMale, 5], [berserk(Enemies.centaurFemale), 1], [berserk(Enemies.centaurMale), 1]], 4)
@@ -29,33 +29,33 @@ const ENENMYENCOUNTERS: Partial<Record<enemyWaveName, Encounter>> = {
 		.waitForEnemiesCleared()
 		.stop()
 	,
-	ELVES: new Encounter()
-		.addWave([[Enemies.leprechaun, 30]], 5)
-		.addWave([[Enemies.leprechaun, 20], [Enemies.woodElfMale, 5], [Enemies.woodElfFemale, 5]], 5)
-		.addWave([[Enemies.leprechaun, 10], [Enemies.woodElfMale, 10], [Enemies.woodElfFemale, 10]], 5)
+	ELVES: () => new Encounter()
+		.addWave([[Enemies.leprechaun, 20], [berserk(Enemies.leprechaun), 5]], 5)
+		.addWave([[Enemies.leprechaun, 20], [berserk(Enemies.leprechaun), 5], [Enemies.woodElfMale, 5], [Enemies.woodElfFemale, 5]], 5)
+		.addWave([[Enemies.leprechaun, 10], [berserk(Enemies.woodElfMale), 5], [Enemies.woodElfMale, 10], [Enemies.woodElfFemale, 10], [berserk(Enemies.woodElfFemale), 5]], 4)
 		.addWave([[Enemies.elfKnight, 5], [Enemies.elfRanger, 3], [Enemies.leprechaun, 10], [Enemies.woodElfMale, 5], [Enemies.woodElfFemale, 5]], 3)
 		.waitForEnemiesCleared()
 		.addGroup(Enemies.elfKing, Enemies.elfKnight, 8, 50)
 		.addGroup(Enemies.elfQueen, Enemies.elfKnight, 8, 50)
 		.waitForEnemiesCleared()
 		.stop(),
-	VILLAGERS: new Encounter()
-		.addWave([[Enemies.villagerFemale, 15], [Enemies.villagerMale, 15]], 5)
-		.addWave([[Enemies.villagerFemale, 10], [Enemies.villagerMale, 10], [Enemies.herald, 10]], 5)
-		.addWave([[Enemies.shopKeeper, 5], [Enemies.mage, 5], [Enemies.villagerFemale, 10], [Enemies.villagerMale, 10]], 5)
-		.addWave([[Enemies.alchemist, 5], [Enemies.blacksmith, 5], [Enemies.butcher, 5], [Enemies.villagerFemale, 15], [Enemies.villagerMale, 15]], 5)
+	VILLAGERS: () => new Encounter()
+		.addWave([[Enemies.villagerFemale, 10], [berserk(Enemies.villagerFemale), 3], [Enemies.villagerMale, 10], [berserk(Enemies.villagerMale), 3]], 5)
+		.addWave([[berserk(Enemies.villagerFemale), 3], [berserk(Enemies.villagerMale), 3], [Enemies.villagerFemale, 5], [Enemies.villagerMale, 5], [Enemies.shopKeeper, 5], [berserk(Enemies.shopKeeper), 2]], 5)
+		.addWave([[Enemies.butcher, 5], [berserk(Enemies.shopKeeper), 5], [Enemies.villagerFemale, 10], [Enemies.villagerMale, 10]], 5)
+		.addWave([[Enemies.blacksmith, 5], [Enemies.shopKeeper, 5], [Enemies.butcher, 5], [Enemies.villagerFemale, 15], [Enemies.villagerMale, 15]], 5)
 		.waitForEnemiesCleared()
-		.addWave([[Enemies.executioner, 1], [Enemies.alchemist, 5], [Enemies.blacksmith, 5], [Enemies.butcher, 5]], 1)
+		.addWave([[Enemies.executioner, 1], [Enemies.shopKeeper, 5], [Enemies.blacksmith, 5], [Enemies.butcher, 5]], 1)
 		.waitForEnemiesCleared()
 		.stop(),
-	PRIESTS: new Encounter()
+	PRIESTS: () => new Encounter()
 		.addWave([[Enemies.angel, 30], [Enemies.clericFat, 5], [Enemies.clericNormal, 5], [Enemies.clericSkinny, 5]], 5)
 		.addWave([[Enemies.clericFat, 10], [Enemies.clericNormal, 10], [Enemies.clericSkinny, 10]], 5)
 		.addWave([[Enemies.nunFat, 10], [Enemies.nunNormal, 10], [Enemies.nunSkinny, 10]], 5)
 		.addGroup(Enemies.bishop, Enemies.angel, 8, 50)
 		.waitForEnemiesCleared()
 		.stop(),
-	UNDEAD: new Encounter()
+	UNDEAD: () => new Encounter()
 		.addWave([[Enemies.zombieSmall, 30]], 5)
 		.addWave([[Enemies.muddy, 5], [Enemies.swampy, 5], [Enemies.zombieSmall, 20]], 5)
 		.addWave([[Enemies.muddy, 10], [Enemies.swampy, 10], [Enemies.zombie, 5], [Enemies.zombieSmall, 10]], 5)
@@ -65,7 +65,7 @@ const ENENMYENCOUNTERS: Partial<Record<enemyWaveName, Encounter>> = {
 		.addWave([[Enemies.ogre, 1], [Enemies.skeleton, 10], [Enemies.zombie, 10], [Enemies.zombieSmall, 20]], 1)
 		.waitForEnemiesCleared()
 		.stop(),
-	KINGSGUARD: new Encounter()
+	KINGSGUARD: () => new Encounter()
 		.addWave([[Enemies.knight, 20]], 3)
 		.addWave([[Enemies.knightFat, 10], [Enemies.knight, 15]], 4)
 		.addWave([[Enemies.knight, 10], [Enemies.knightFat, 10], [Enemies.knightElite, 10]], 4)
@@ -74,7 +74,7 @@ const ENENMYENCOUNTERS: Partial<Record<enemyWaveName, Encounter>> = {
 		.addGroup(Enemies.queen, Enemies.knightElite, 8, 50)
 		.waitForEnemiesCleared()
 		.stop(),
-	DEMONS: new Encounter()
+	DEMONS: () => new Encounter()
 		.addWave([[Enemies.imp, 40]], 5)
 		.addWave([[Enemies.imp, 30], [Enemies.wogol, 10]], 5)
 		.addWave([[Enemies.chort, 10], [Enemies.wogol, 10], [Enemies.imp, 20]], 5)
