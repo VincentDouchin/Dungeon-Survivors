27754e69647ca4addaba02aeb741f4ab4f053a83
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-01 08:46:36 +0100

    clean up

--- src/Globals/InputManager.ts
+++ src/Globals/InputManager.ts
@@ -69,7 +69,7 @@ class InputManager {
 					}
 
 					if (event instanceof TouchEvent) {
-						Array.from(event.touches.length ? event.changedTouches : event.changedTouches).forEach((touch) => sendEvent(touch))
+						Array.from(event.changedTouches).forEach((touch) => sendEvent(touch))
 					} else {
 						sendEvent(event)
 					}
