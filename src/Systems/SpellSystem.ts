face49cd264195dcfc648e9626ee32a1a80f3d2c
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-02 19:52:28 +0100

    change spell to accept entity

--- src/Systems/SpellSystem.ts
+++ src/Systems/SpellSystem.ts
@@ -2,10 +2,8 @@ import { ECS, Entity, System } from "../Globals/ECS";
 import ECSEVENTS, { MANA_AMOUNT, MANA_PERCENT } from "../Constants/ECSEvents";
 
 import ManaComponent from "../Components/ManaComponent";
-import PositionComponent from "../Components/PositionComponent";
 import { SKILL } from "../Constants/InputsNames";
 import SpellComponent from "../Components/SpellComponent";
-import StatsComponent from "../Components/StatsComponent";
 import SwitchingComponent from "../Components/SwitchingComponent";
 import { inputManager } from "../Globals/Initialize";
 
@@ -19,12 +17,10 @@ class SpellSystem extends System {
 			.forEach(entity => {
 				const switcher = entity.getComponent(SwitchingComponent)
 				if (attack && switcher && !switcher?.main) {
-					const position = entity.getComponent(PositionComponent)
 					const spell = entity.getComponent(SpellComponent)
 					const mana = entity.getComponent(ManaComponent)
-					const stats = entity.getComponent(StatsComponent)
 					if (mana.mana >= mana.manaCost) {
-						spell.spell(position.position, stats)
+						spell.spell(entity)
 						mana.mana -= mana.manaCost
 						ECS.eventBus.publish<MANA_PERCENT>(ECSEVENTS.MANA_PERCENT, mana.mana / mana.maxMana.value)
 						ECS.eventBus.publish<MANA_AMOUNT>(ECSEVENTS.MANA_AMOUNT, mana.mana)
