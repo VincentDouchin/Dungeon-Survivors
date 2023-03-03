a12b4116033fc383670884e88c16306c92b670da
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-03 19:20:41 +0100

    correcly stop coroutines

--- src/Globals/Coroutine.ts
+++ src/Globals/Coroutine.ts
@@ -1,23 +1,30 @@
+import Engine from "./Engine"
+import { GameStates } from "../Constants/GameStates"
 
 class Coroutine {
 	static coroutines: Coroutine[] = []
 
 	static run() {
-		for (let i = this.coroutines.length - 1; i >= 0; i--) {
-			if (!this.coroutines[i].running) continue
-			const { done } = this.coroutines[i].generator.next()
+		if (Engine.stateName === GameStates.pause) debugger
+		for (let i = Coroutine.coroutines.length - 1; i >= 0; i--) {
+			if (!Coroutine.coroutines[i].running) continue
+			if (Coroutine.coroutines[i].toStop) {
+				Coroutine.coroutines.splice(i, 1)
+			}
+			const { done } = Coroutine.coroutines[i].generator.next()
 			if (done) {
-				this.coroutines.splice(i, 1)
+				Coroutine.coroutines.splice(i, 1)
 			}
 		}
 	}
 	generator: Generator
 	running: boolean = true
+	toStop = false
 	constructor(fn: (args: number) => Generator, duration?: number) {
 		const generatorFunction =
 			duration
 				? function* () {
-					for (let i = 0; i < duration; i++) {
+					for (let i = 0; i < duration - 1; i++) {
 						yield* fn(i)
 					}
 				}
@@ -36,7 +43,7 @@ class Coroutine {
 		this.running = false
 	}
 	stop() {
-		Coroutine.coroutines.splice(Coroutine.coroutines.indexOf(this), 1)
+		this.toStop = true
 	}
 
 }
