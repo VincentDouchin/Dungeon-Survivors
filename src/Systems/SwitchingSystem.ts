ddf02bcc0bedaaa17c8ec99e6b740ab19942fb64
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-04 14:53:17 +0100

    fix switching system

--- src/Systems/SwitchingSystem.ts
+++ src/Systems/SwitchingSystem.ts
@@ -19,9 +19,15 @@ class SwitchingSystem extends System {
 	}
 	update(entities: Entity[]) {
 		const toSwitch = inputManager.getInput(INPUTS.SWITCH)!.once
+		const canSwitch = entities.length > 1
+		const needsSwitch = !entities.some(entity => {
+			const switcher = entity.getComponent(SwitchingComponent)
+			return switcher.initiated && !switcher.main
+		})
+		if (!canSwitch && !needsSwitch) return
 		entities.forEach(entity => {
 			const switcher = entity.getComponent(SwitchingComponent)
-			if (toSwitch || !switcher.initiated) {
+			if (toSwitch || !switcher.initiated || (!canSwitch && needsSwitch)) {
 				if (!switcher.initiated) switcher.initiated = true
 				const flocking = entity.getComponent(FlockingComponent)
 				const spell = entity.getComponent(SpellComponent)
