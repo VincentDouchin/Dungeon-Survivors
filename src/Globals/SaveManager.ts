fd5b60ddf975fdc0348848ddf7af79d3ec457e94
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-04 14:41:46 +0100

    reset save if updated

--- src/Globals/SaveManager.ts
+++ src/Globals/SaveManager.ts
@@ -2,14 +2,33 @@ import { HeroName } from "../Constants/Heros"
 
 const blankSave: {
 	heros: HeroName[]
-	volume: number
+	effectsVolume: number
+	musicVolume: number
+	zoom: number
 } = {
 	heros: [],
-	volume: 0.1,
+	effectsVolume: 0.1,
+	musicVolume: 0.1,
+	zoom: 700
 }
 const localSave = localStorage.getItem('save')
-const saveData = localSave ? JSON.parse(localSave) as typeof blankSave : blankSave
+const getSaveData = () => {
+	if (localSave) {
+		const parsedSave = JSON.parse(localSave)
+		if (Object.keys(parsedSave).every(key => Object.keys(blankSave).includes(key))) {
+			return parsedSave
+		} else {
+			return { ...blankSave }
+		}
+
+	} else {
+		return { ...blankSave }
+	}
+}
+const saveData = getSaveData()
+
 export const save = () => {
 	localStorage.setItem('save', JSON.stringify(saveData))
 }
+save()
 export default saveData
