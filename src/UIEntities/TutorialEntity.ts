4ac6b59d1d00d3f2dc22e16b563123282fb1fb69
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-04 11:35:12 +0100

    remove auto destroy

--- src/UIEntities/TutorialEntity.ts
+++ src/UIEntities/TutorialEntity.ts
@@ -1,20 +1,14 @@
-import Coroutine from "../Globals/Coroutine";
-import { Entity } from "../Globals/ECS"
-import SpriteComponent from "../Components/SpriteComponent"
-import TextComponent from "../Components/TextComponent"
-import Tile from "../Utils/Tile"
-import UIPositionComponent from "../Components/UIPositionComponent"
-import waitFor from "../Utils/WaitFor"
+import { Entity } from "../Globals/ECS";
+import SpriteComponent from "../Components/SpriteComponent";
+import TextComponent from "../Components/TextComponent";
+import Tile from "../Utils/Tile";
+import UIPositionComponent from "../Components/UIPositionComponent";
 
 const TutorialEntity = () => {
 	const tutorial = new Entity('tutorial')
 	tutorial.addComponent(new SpriteComponent(Tile.empty()))
 	tutorial.addComponent(new TextComponent('WASD - Movement     Shift - Switch charcaters     P - Spell     Escape - Pause'))
 	tutorial.addComponent(new UIPositionComponent({ x: 0, y: -1 }, { x: 0, y: -1 }))
-	new Coroutine(function* () {
-		yield* waitFor(600)
-		tutorial.destroy()
-	})
 	return tutorial
 }
 export default TutorialEntity
