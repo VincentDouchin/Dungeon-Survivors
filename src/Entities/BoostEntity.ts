f1151fad123cb22bd400a87b5f0551e8ed8d424e
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-04 11:34:02 +0100

    add outline to boosts

--- src/Entities/BoostEntity.ts
+++ src/Entities/BoostEntity.ts
@@ -3,11 +3,12 @@ import { Boost } from "../Constants/Boosts";
 import BoostComponent from "../Components/BoostComponent";
 import COLLISIONGROUPS from "../Constants/CollisionGroups";
 import { Entity } from "../Globals/ECS";
+import OutlineShader from "../Shaders/OutlineShader";
 import SpriteComponent from "../Components/SpriteComponent";
 
 const BoostEntity = (boostDefinition: Boost) => () => {
 	const boost = new Entity('boost')
-	boost.addComponent(new SpriteComponent(boostDefinition.tile, { scale: 0.8, renderOrder: 0 }))
+	boost.addComponent(new SpriteComponent(boostDefinition.tile, { scale: 0.8, renderOrder: 0, shaders: [new OutlineShader([1, 1, 1, 1])] }))
 	boost.addComponent(new BoostComponent(boostDefinition))
 	boost.addComponent(new BodyComponent({ type: 'fixed' }, [
 		{ width: boostDefinition.tile.width, height: boostDefinition.tile.height, sensor: true, contact: false, group: COLLISIONGROUPS.POTION, canCollideWith: [COLLISIONGROUPS.PLAYER] }
