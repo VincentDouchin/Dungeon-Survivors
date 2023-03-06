1c48a8322c0db14f87d0131672e90533f90a512a
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-06 09:14:55 +0100

    add lootables

--- src/Systems/BackgroundElementSpawnerSystem.ts
+++ src/Systems/BackgroundElementSpawnerSystem.ts
@@ -1,6 +1,7 @@
 import { Entity, System } from "../Globals/ECS";
 
 import BackgroundElementsComponent from "../Components/BackgroundElementsComponent";
+import LootableEntity from "../Entities/LootableEntity";
 import ObstableEntity from "../Entities/ObstacleEntity";
 import PositionComponent from "../Components/PositionComponent";
 import { camera } from "../Globals/Initialize";
@@ -12,7 +13,7 @@ class BackgroundElementSpawnerSystem extends System {
 	update(entities: Entity[]): void {
 		entities.forEach(entity => {
 			const backgroundElements = entity.getComponent(BackgroundElementsComponent)
-			const { size, obstacles, noise, obstaclesEntities, obstaclesDensity, effect, effectDelay } = backgroundElements
+			const { size, obstacles, noise, obstaclesEntities, obstaclesDensity, effect, effectDelay, lootables, } = backgroundElements
 			if (effect && effectDelay) {
 				if (backgroundElements.effectsTimer < 0) {
 					entity.addChildren(effect())
@@ -30,21 +31,21 @@ class BackgroundElementSpawnerSystem extends System {
 			}
 			for (let x = camera.left + camera.position.x - size; x < camera.right + camera.position.x + size; x += size) {
 				for (let y = camera.bottom + camera.position.y - size; y < camera.top + camera.position.y + size; y += size) {
+
 					const chunkX = Math.floor(x / size)
 					const chunkY = Math.floor(y / size)
 					const noiseValue = noise(chunkX, chunkY)
 					if (Math.abs(noiseValue) > (1 - obstaclesDensity)) {
 						const newNoise = noise(chunkX + noiseValue, chunkY + noiseValue)
-						const obstacleTile = obstacles[Math.floor((newNoise + 1) / 2 * obstacles.length)]
+						const getRandom = (arr: any[]) => arr[Math.floor(Math.abs(newNoise) / 0.84 * arr.length)]
 						if (!obstaclesEntities[`${chunkX}|${chunkY}`]) {
-							const obstacle = ObstableEntity(obstacleTile)(chunkX * size, chunkY * size)
+							const obstacleEntity = Math.abs(noise(newNoise, obstaclesDensity)) > 0.3 ? ObstableEntity(getRandom(obstacles)) : LootableEntity(getRandom(lootables))
+							const obstacle = obstacleEntity(chunkX * size, chunkY * size)
 							obstaclesEntities[`${chunkX}|${chunkY}`] = obstacle
 							entity.addChildren(obstacle)
 						}
 					}
 				}
-
-
 			}
 		})
 	}
