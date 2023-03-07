221f7aa5d279488c20b92e55713d94633c1f2678
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-07 18:42:20 +0100

    update dissolve shader to use duration

--- src/Shaders/DissolveShader.ts
+++ src/Shaders/DissolveShader.ts
@@ -1,6 +1,5 @@
 import Coroutine from '../Globals/Coroutine'
 import Shader from './Shader'
-import { clock } from '../Globals/Initialize'
 import frag from './glsl/dissolve.frag?raw'
 import noise from './glsl/lib/cnoise.glsl?raw'
 import vert from './glsl/main.vert?raw'
@@ -9,21 +8,24 @@ class DissolveShader extends Shader {
 	vert = vert
 	frag = noise + frag
 	finish: null | Promise<void> = null
-	constructor(duration: number) {
+	constructor(duration: number, invert: boolean = false, kernel: number) {
 		super((sprite) => {
 			this.finish = new Promise<void>(resolve => {
 				new Coroutine(function* () {
-					const timestamp = clock.getElapsedTime()
 					for (let i = 0; i < duration; i++) {
 						yield
-						sprite.uniforms.time = clock.getElapsedTime() - timestamp
+						sprite.getUniforms(DissolveShader).time.value = i / duration
+						sprite.render()
 					}
 					resolve()
 				})
 			})
 			return {
-				time: clock.getElapsedTime(),
-				seed: Math.random()
+				time: 0,
+				seed: Math.random(),
+				invert: invert,
+				duration,
+				kernel
 			}
 		})
 	}
--- src/Shaders/glsl/dissolve.frag
+++ src/Shaders/glsl/dissolve.frag
@@ -1,10 +1,12 @@
 uniform sampler2D tDiffuse;
 uniform float time;
 uniform float seed;
+uniform bool invert;
+uniform float kernel;
 varying vec2 vUv;
 
 void main() {
-	float noise = cnoise(vec3((vUv.xy + vec2(seed)) * vec2(5.), 0.));
+	float noise = cnoise(vec3((vUv.xy + vec2(seed)) * vec2(kernel), 99.));
 	vec4 pixelColor = texture2D(tDiffuse, vUv);
-	gl_FragColor = noise < (1. - time * 3.) ? pixelColor : vec4(0);
+	gl_FragColor = (invert ? (noise < time) : (noise > time)) ? pixelColor : vec4(0);
 }
