bf93d5727673d4898aabae9914bd8f727c3257f0
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-07 18:43:31 +0100

    improve obstacle spawning

--- src/Systems/BackgroundElementSpawnerSystem.ts
+++ src/Systems/BackgroundElementSpawnerSystem.ts
@@ -1,21 +1,18 @@
-import { ECS, Entity, System } from "../Globals/ECS";
+import { Entity, System } from "../Globals/ECS";
 
 import BackgroundElementsComponent from "../Components/BackgroundElementsComponent";
-import DroppableComponent from "../Components/DroppableComponent";
-import { ECSEVENTS } from "../Constants/Events";
-import LootableEntity from "../Entities/LootableEntity";
-import ObstableEntity from "../Entities/ObstacleEntity";
-import PositionComponent from "../Components/PositionComponent";
 import { camera } from "../Globals/Initialize";
 
 class BackgroundElementSpawnerSystem extends System {
+
 	constructor() {
 		super(BackgroundElementsComponent)
 	}
 	update(entities: Entity[]): void {
 		entities.forEach(entity => {
+
 			const backgroundElements = entity.getComponent(BackgroundElementsComponent)
-			const { size, obstacles, noise, obstaclesEntities, obstaclesDensity, effect, effectDelay, lootables, } = backgroundElements
+			const { size, effect, effectDelay, obstaclesMap } = backgroundElements
 			if (effect && effectDelay) {
 				if (backgroundElements.effectsTimer < 0) {
 					entity.addChildren(effect())
@@ -23,29 +20,37 @@ class BackgroundElementSpawnerSystem extends System {
 				}
 				backgroundElements.effectsTimer--
 			}
-			for (const [key, obstacle] of Object.entries(obstaclesEntities)) {
-				const position = obstacle.getComponent(PositionComponent)
-				if (!position) continue
-				if (position.x > camera.position.x + camera.right * 2 || position.x < camera.position.x + camera.left * 2 || position.y > camera.position.y + camera.top * 2 || position.y < camera.position.y + camera.bottom * 2) {
-					delete obstaclesEntities[key]
-					obstacle.removeComponent(DroppableComponent)?.destroy()
-					obstacle.destroy()
+			obstaclesMap.forEach((node) => {
+				if (node.entity) {
+
+					if (!node.position) return
+					if (
+						node.position.x > camera.position.x + camera.right * 2
+						|| node.position.x < camera.position.x + camera.left * 2
+						|| node.position.y > camera.position.y + camera.top * 2
+						|| node.position.y < camera.position.y + camera.bottom * 2
+					) {
+						node.entity.destroy()
+						node.position = null
+						node.entity = null
+					}
 				}
-			}
+			})
+
 			for (let x = camera.left + camera.position.x - size; x < camera.right + camera.position.x + size; x += size) {
 				for (let y = camera.bottom + camera.position.y - size; y < camera.top + camera.position.y + size; y += size) {
-
 					const chunkX = Math.floor(x / size)
 					const chunkY = Math.floor(y / size)
-					const noiseValue = noise(chunkX, chunkY)
-					if (Math.abs(noiseValue) > (1 - obstaclesDensity)) {
-						const newNoise = noise(chunkX + noiseValue, chunkY + noiseValue)
-						const getRandom = (arr: any[]) => arr[Math.floor(Math.abs(newNoise) / 0.84 * arr.length)]
-						if (!obstaclesEntities[`${chunkX}|${chunkY}`]) {
-							const obstacleEntity = Math.abs(noise(newNoise, obstaclesDensity)) > 0.1 ? ObstableEntity(getRandom(obstacles)) : LootableEntity(getRandom(lootables))
-							const obstacle = obstacleEntity(chunkX * size, chunkY * size)
-							obstaclesEntities[`${chunkX}|${chunkY}`] = obstacle
-							ECS.eventBus.publish(ECSEVENTS.ADD_WALL, obstacle)
+					const node = backgroundElements.getNode(chunkX, chunkY)
+					if (!node) {
+
+						backgroundElements.createNode(chunkX, chunkY)
+
+					}
+
+					if (node && node.obstacle && !node.entity) {
+						const obstacle = backgroundElements.createEntity(chunkX, chunkY)
+						if (obstacle) {
 							entity.addChildren(obstacle)
 						}
 					}
