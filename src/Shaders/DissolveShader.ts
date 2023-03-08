22b71913d90730a279a2a307713c946fc8b2edbc
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-08 06:07:07 +0100

    fix dissolve shader

--- src/Shaders/DissolveShader.ts
+++ src/Shaders/DissolveShader.ts
@@ -7,19 +7,14 @@ import vert from './glsl/main.vert?raw'
 class DissolveShader extends Shader {
 	vert = vert
 	frag = noise + frag
-	finish: null | Promise<void> = null
+
 	constructor(duration: number, invert: boolean = false, kernel: number) {
 		super((sprite) => {
-			this.finish = new Promise<void>(resolve => {
-				new Coroutine(function* () {
-					for (let i = 0; i < duration; i++) {
-						yield
-						sprite.getUniforms(DissolveShader).time.value = i / duration
-						sprite.render()
-					}
-					resolve()
-				})
-			})
+			new Coroutine(function* (i) {
+				yield
+				sprite.getUniforms(DissolveShader).time.value = i / duration
+				sprite.render()
+			}, duration)
 			return {
 				time: 0,
 				seed: Math.random(),
