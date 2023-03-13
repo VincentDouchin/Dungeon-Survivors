d3c94bd150837c7ba61b454807e6dfd39e8e1e67
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-13 08:07:46 +0100

    remove level

--- src/Components/StatsComponent.ts
+++ src/Components/StatsComponent.ts
@@ -22,13 +22,9 @@ export interface BoostModifier {
 class StatsComponent extends Component {
 	stats: Map<STATS, { levelModifier: number, modifier: number }> = new Map()
 	boosts: BoostModifier[] = []
-	xp = 0
-	level = 0
-	nextLevel = 20
 	getemptyStat = () => ({ levelModifier: 0, modifier: 0 })
-	constructor(level = 0) {
+	constructor() {
 		super()
-		this.level = level
 	}
 	set(statName: STATS, levelModifier: number = 0) {
 		this.stats.set(statName, this.getemptyStat())
@@ -41,22 +37,13 @@ class StatsComponent extends Component {
 		}
 		this.stats.get(statName)!.modifier += modifier
 	}
-	get(statName: STATS, base: number) {
+	getModifier(statName: STATS) {
 		const stat = this.stats.get(statName) ?? this.getemptyStat()
 		const boost = this.boosts.reduce((acc, boostModifier) => boostModifier.stat === statName ? acc + boostModifier.modifier : acc, 0)
-		return (base + (base * this.level * stat.levelModifier)) * (1 + stat.modifier + boost)
+		return stat.modifier + boost
 	}
-	updateXP(amount: number) {
-		this.xp += amount
-		const levelUp = Math.floor(this.xp / this.nextLevel)
-		if (levelUp > 0) {
-			for (let i = 0; i < levelUp; i++) {
-				this.xp = this.xp % this.nextLevel
-				this.nextLevel = Math.ceil(10 * (1 + Math.log(this.level + 2) * 5))
-				this.level++
-			}
-		}
-		return this.level
+	getLevelModifier(statName: STATS) {
+		return this.stats.get(statName)?.levelModifier ?? 0
 	}
 
 }
