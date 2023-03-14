acc6593cb7caec8eef44803460ffa929310fd43f
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-14 16:57:48 +0000

    fix stat calculation?

--- src/Game/Stat.ts
+++ src/Game/Stat.ts
@@ -16,7 +16,9 @@ class Stat {
 		this.modifier = modifier
 	}
 	get value() {
-		return this.base * (((this.level?.level ?? 1) * (this.modifier?.getLevelModifier(this.name) ?? 1)) + (this.modifier?.getModifier(this.name) ?? 0))
+		const levelModifer = this.base * ((this.level?.level ?? 0) * (this.modifier?.getLevelModifier(this.name) ?? 0))
+		const statsModifier = this.base * (this.modifier?.getModifier(this.name) ?? 0)
+		return this.base + levelModifer + statsModifier
 	}
 }
 
