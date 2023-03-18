249834aaf2b156388020b6880143ce76ea4b0c0d
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-18 17:19:31 +0100

    check if entity wasn't detroyed in previous loop

--- src/Systems/ExpirationSystem.ts
+++ src/Systems/ExpirationSystem.ts
@@ -1,19 +1,26 @@
 import { Entity, System } from "../Globals/ECS";
 
+import { ECSEVENTS } from "../Constants/Events";
 import ExpirationComponent from "../Components/ExpirationComponent";
 
 class ExpirationSystem extends System {
+	skipEntities = new Set<Entity>()
 	constructor() {
 		super(ExpirationComponent)
+		this.subscribe(ECSEVENTS.DELETE_ENTITY, (entity) => {
+			this.skipEntities.add(entity)
+		})
 	}
 	update(entities: Entity[]) {
 		entities.forEach(entity => {
+			if (this.skipEntities.has(entity)) return
 			const timer = entity.getComponent(ExpirationComponent)
 			timer.timer--
 			if (timer.timer === 0) {
 				entity.destroy()
 			}
 		})
+		this.skipEntities.clear()
 	}
 }
 export default ExpirationSystem
