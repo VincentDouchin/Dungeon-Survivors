6e3ae31374a19760352110cf7668ef34bd9a8337
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-19 11:34:07 +0100

    remove enemy level display

--- src/UIEntities/TimeCounterEntity.ts
+++ src/UIEntities/TimeCounterEntity.ts
@@ -1,6 +1,6 @@
 import { ECS, Entity } from "../Globals/ECS";
-import { ECSEVENTS, UIEVENTS } from "../Constants/Events";
 
+import { ECSEVENTS } from "../Constants/Events";
 import SpriteComponent from "../Components/SpriteComponent";
 import State from "../Globals/State";
 import TextComponent from "../Components/TextComponent";
@@ -14,21 +14,11 @@ const TimeCounterEntity = () => {
 	timer.addComponent(new SpriteComponent(assets.UI.frame2.framed(8, 24, 0), { scale: 1.5 }))
 	const position = timer.addComponent(new UIPositionComponent({ x: 0, y: 2 }, { x: 0, y: 1 }))
 	position.moveTo(1, 20)
-
 	const timerText = timer.addComponent(new TextComponent(formatTimer(),))
-	const enemyLevel = new Entity('enemy level')
-	enemyLevel.addComponent(new SpriteComponent(assets.icons.skull, { scale: 2 }))
-	enemyLevel.addComponent(new UIPositionComponent({ x: 1, y: 0 }, { x: -1, y: 0 }))
-	const enemyLevelText = enemyLevel.addComponent(new TextComponent(String(0), { outlineWidth: 1 }))
 	const timerSub = ECS.eventBus.subscribe(ECSEVENTS.TIMER, (_) => {
 		timerText.setText(formatTimer())
 	})
-	const enemyLevelSub = ECS.eventBus.subscribe(UIEVENTS.ENEMY_LEVEL, (level) => {
-		enemyLevelText.setText(String(level))
-	})
-	timer.addChildren(enemyLevel)
 	timer.onDestroy(() => {
-		enemyLevelSub()
 		timerSub()
 	})
 	return timer
