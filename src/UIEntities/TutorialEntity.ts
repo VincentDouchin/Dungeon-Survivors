896227a0c74782cc3ccc298ddcfeaa89e7adb14c
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-20 13:04:30 +0100

    fix tutorial

--- src/UIEntities/TutorialEntity.ts
+++ src/UIEntities/TutorialEntity.ts
@@ -6,8 +6,8 @@ import UIPositionComponent from "../Components/UIPositionComponent";
 
 const TutorialEntity = () => {
 	const tutorial = new Entity('tutorial')
-	tutorial.addComponent(new SpriteComponent(Tile.empty()))
-	tutorial.addComponent(new TextComponent('WASD - Movement     Shift - Switch charcaters     P - Spell     Escape - Pause'))
+	tutorial.addComponent(new SpriteComponent(Tile.empty(), { renderOrder: 0 }))
+	tutorial.addComponent(new TextComponent('WASD - Movement     Shift - Switch charcaters     Space - Spell     Escape - Pause'))
 	tutorial.addComponent(new UIPositionComponent({ x: 0, y: -1 }, { x: 0, y: -1 }))
 	return tutorial
 }
