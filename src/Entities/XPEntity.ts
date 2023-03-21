66493d16df6d34c0268f5c06aeb250adf9a4fe2a
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-21 12:49:48 +0100

    split xp

--- src/Entities/XPEntity.ts
+++ src/Entities/XPEntity.ts
@@ -8,16 +8,29 @@ import XPComponent from "../Components/XPComponent"
 import assets from "../Globals/Assets"
 
 const XPEntity = (amount = 1) => (position: PositionComponent) => {
-	const xp = new Entity('xp')
-	xp.addComponent(new SpriteComponent(assets.effects.Spark, { renderOrder: 1, scale: 0.8 * (1 + amount / 10) }))
-	xp.addComponent(new AnimationComponent({ idle: assets.effects.Spark }))
-	xp.addComponent(new BodyComponent(
-		{ moveForce: 1000 },
-		[
-			{ width: 2, height: 2, contact: false, group: COLLISIONGROUPS.XP, canCollideWith: [COLLISIONGROUPS.SENSOR], mass: 1 }
-		]))
-	xp.addComponent(new XPComponent(amount))
-	xp.addComponent(position)
-	return xp
+	const xpBag = new Entity('xp bag')
+	let counter = amount
+	const getOffset = () => (Math.random() - 0.5) * 2 * amount
+	for (let i = 0; i < amount; i++) {
+		const xp = new Entity('xp')
+		xp.addComponent(new SpriteComponent(assets.effects.Spark, { renderOrder: 1, scale: 0.8 }))
+		xp.addComponent(new AnimationComponent({ idle: assets.effects.Spark }))
+		xp.addComponent(new BodyComponent(
+			{ moveForce: 1000 },
+			[
+				{ width: 2, height: 2, contact: false, group: COLLISIONGROUPS.XP, canCollideWith: [COLLISIONGROUPS.SENSOR], mass: 1 }
+			]))
+		xp.addComponent(new XPComponent(1))
+		xp.addComponent(new PositionComponent(position.x + getOffset(), position.y + getOffset()))
+		xp.onDestroy(() => {
+			counter--
+			if (counter === 0) {
+				xpBag.destroy()
+			}
+		})
+		xpBag.addChildren(xp)
+
+	}
+	return xpBag
 }
 export default XPEntity
