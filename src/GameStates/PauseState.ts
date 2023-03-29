ab5644e6f69b00de52884c05bbd9e2e86d9136fd
Vincent Douchin <vincent.douchin@gmail.com>
2023-03-29 08:23:19 +0200

    fix pause

--- src/GameStates/PauseState.ts
+++ src/GameStates/PauseState.ts
@@ -1,25 +1,20 @@
-import { engine, inputManager, render } from '../Globals/Initialize'
+import { render } from '../Globals/Initialize'
 
 import CameraSystem from '../Systems/CameraSystem'
 import { ECS } from '../Globals/ECS'
 import type { Entity } from '../Globals/ECS'
 import type { GameState } from '../Globals/Engine'
-import INPUTS from '../Constants/InputsNames'
 import RenderSystem from '../Systems/RenderSystem'
 import SelectionSystem from '../Systems/SelectionSystem'
 import State from '../Globals/State'
 import TutorialEntity from '../UIEntities/TutorialEntity'
 import UIPauseEntity from '../UIEntities/UIPauseEntity'
-import RunState from './RunState'
 
 class PauseState implements GameState {
 	ui?: Entity
 	tutorial?: Entity
 	update() {
 		ECS.updateSystems()
-		if (inputManager.getInput(INPUTS.PAUSE).once) {
-			engine.setState(RunState)
-		}
 	}
 
 	render() {
