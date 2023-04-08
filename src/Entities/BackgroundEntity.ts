14fd157e480682ba67891d99faee923d2538681f
Vincent Douchin <vincent.douchin@gmail.com>
2023-04-08 16:45:26 +0200

    clean up

--- src/Entities/BackgroundEntity.ts
+++ src/Entities/BackgroundEntity.ts
@@ -41,18 +41,6 @@ const BackgroundEntity = (backgroundDefinition: BackgroundOptions) => {
 		top: backgroundDefinition.infinite.y ? undefined : height / 2,
 	}
 
-	// if (backgroundDefinition?.obstacles?.length || backgroundDefinition.lootables.length) {
-	// 	const walls = level
-	// 		?.layerInstances
-	// 		?.find((layer: LayerInstance) => layer.__identifier == 'Wall_entities')
-	// 		?.entityInstances
-	// 		.map(wall => ({
-	// 			width: wall.width,
-	// 			height: wall.height,
-	// 			x: wall.px[0] - level.pxWid / 2 + wall.width / 2,
-	// 			y: level.pxHei / 2 - wall.px[1] - wall.height / 2
-	// 		}))
-	// }
 	background.addComponent(new BackgroundElementsComponent({
 		obstacles: backgroundDefinition.obstacles,
 		effect: backgroundDefinition.effect,
@@ -60,22 +48,11 @@ const BackgroundEntity = (backgroundDefinition: BackgroundOptions) => {
 		lootables: backgroundDefinition.lootables,
 		obstacleDensity: backgroundDefinition.obstacleDensity,
 	}))
+
 	background.onDestroy(() => {
 		cameraUnSubscriber()
 		addUnSubscriber()
 	})
-
-	// ! WALLS
-	// level?.layerInstances?.find((layer: LayerInstance) => layer.__identifier == 'Wall_entities')?.entityInstances.forEach(wall => {
-	// 	const wallEntity = new Entity('wall')
-	// 	wallEntity.addComponent(new BodyComponent(
-	// 		{ type: 'fixed' },
-	// 		[
-	// 			{ width: wall.width, height: wall.height, contact: false, group: COLLISIONGROUPS.WALL, canCollideWith: [COLLISIONGROUPS.PLAYER, COLLISIONGROUPS.ENEMY] }]
-	// 	))
-	// 	wallEntity.addComponent(new PositionComponent(wall.px[0] - level.pxWid / 2 + wall.width / 2, level.pxHei / 2 - wall.px[1] - wall.height / 2))
-	// 	background.addChildren(wallEntity)
-	// })
 	if (backgroundDefinition.lightColor) {
 		background.addComponent(new LightComponent(backgroundDefinition.lightColor, 1, AmbientLight))
 	}
