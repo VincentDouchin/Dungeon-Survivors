ea987e27895311f9084f95358cad7351c52f3799
Vincent Douchin <vincent.douchin@gmail.com>
2023-04-27 18:08:07 +0200

    improve sound

--- src/Globals/SoundManager.ts
+++ src/Globals/SoundManager.ts
@@ -12,6 +12,7 @@ interface soundManagerOptions {
 }
 class SoundManager {
 	sounds: Record<sounds, HTMLAudioElement>
+	availableSounds: Partial<Record<sounds, MediaElementAudioSourceNode[]>> = {}
 	ctx = new AudioContext()
 	effects: Map<HTMLAudioElement, number> = new Map()
 	musics: Map<HTMLAudioElement, number> = new Map()
@@ -36,39 +37,33 @@ class SoundManager {
 	}
 
 	play(type: 'music' | 'effect', name: SOUND, options?: soundManagerOptions) {
-		const nodes: AudioNode[] = []
+		if (this.ctx.state === 'suspended') return
 		const newOptions = { volume: 1, playbackRate: 1, autoplay: true, loop: false, fade: false, ...options }
 		const selectedSound = name[Math.floor(Math.random() * name.length)]
-		const audioElement = this.sounds[selectedSound].cloneNode() as HTMLAudioElement
+		const existingSound = this.availableSounds[selectedSound]?.pop()?.mediaElement
+		const audioElement = existingSound ?? this.sounds[selectedSound].cloneNode() as HTMLAudioElement
 		const target = type === 'music' ? this.musics : this.effects
 		const volume = type === 'music' ? saveData.musicVolume : saveData.effectsVolume
 
 		target.set(audioElement, newOptions.volume)
-		const sound = this.ctx.createMediaElementSource(audioElement)
-		nodes.push(sound)
-		const endListener = () => {
-			target.delete(audioElement)
-			audioElement.remove()
-			sound.disconnect()
-			audioElement.src = ''
-			audioElement.srcObject = null
-			audioElement.removeEventListener('ended', endListener)
-		}
-		audioElement.addEventListener('ended', endListener)
+
 		audioElement.volume = newOptions.volume * volume
 		audioElement.playbackRate = newOptions.playbackRate
 		if (newOptions.loop) audioElement.loop = true
+		if (!existingSound) {
+			const sound = this.ctx.createMediaElementSource(audioElement)
+			sound.connect(this.ctx.destination)
+			const endListener = () => {
+				const available = this.availableSounds[selectedSound]
+				if (available) {
+					available.push(sound)
+				} else {
+					this.availableSounds[selectedSound] = [sound]
+				}
+			}
+			audioElement.addEventListener('ended', endListener)
+		}
 
-		if (newOptions.fade) {
-			const gainNode = this.ctx.createGain()
-			gainNode.gain.setValueAtTime(1, this.ctx.currentTime)
-			gainNode.gain.exponentialRampToValueAtTime(0.001, this.ctx.currentTime + this.sounds[selectedSound].duration)
-			nodes.push(gainNode)
-		}
-		for (let i = 0; i < nodes.length; i++) {
-			if (i !== 0) nodes[i - 1].connect(nodes[i])
-			if (i === nodes.length - 1) nodes[i].connect(this.ctx.destination)
-		}
 		if (newOptions.autoplay) audioElement.play()
 		return audioElement
 	}
@@ -83,6 +78,11 @@ class SoundManager {
 				audioElement.volume = elementVolume * volume
 			}
 		})
+		if ((saveData.effectsVolume + saveData.musicVolume) === 0) {
+			this.ctx.suspend()
+		} else if (this.ctx.state === 'suspended') {
+			this.ctx.resume()
+		}
 	}
 }
 export default SoundManager
