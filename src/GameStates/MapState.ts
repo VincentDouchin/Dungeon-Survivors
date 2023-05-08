f9449997cd47f762385bad8fabc9221b2bf587d6
Vincent Douchin <vincent.douchin@gmail.com>
2023-05-08 16:54:27 +0200

    fix saving

--- src/GameStates/MapState.ts
+++ src/GameStates/MapState.ts
@@ -84,6 +84,7 @@ class MapState implements GameState {
 		this.map = new Entity('map')
 		this.map.addComponent(new SpriteComponent(mapTile))
 		this.map.addComponent(new PositionComponent(0, 0))
+
 		let wasEncounter = false
 		if (previousState === null) {
 			// ! CAMERA TARGET
@@ -151,6 +152,8 @@ class MapState implements GameState {
 				}
 			}
 			title.destroy()
+		} else {
+			setProgress({ position: this.lastPosition })
 		}
 		showMap(wasEncounter)
 	}
@@ -160,7 +163,6 @@ class MapState implements GameState {
 		this.ui?.destroy()
 		switch (newState) {
 		default: {
-			setProgress({ position: this.lastPosition })
 			this.lastPosition = this.player?.getComponent(PositionComponent)
 			this.map?.destroy()
 			this.path?.destroy()
